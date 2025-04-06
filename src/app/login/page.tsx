5a1938022f9818f81fa0e2680f29083e6b5ecae4
minbo <smb5174@naver.com>
2025-04-07 00:06:46 +0900

    design: 로그인 페이지 padding 제거
    padding은 RN에서 설정하는 게 좋다고 생각했습니다.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,15 +4,13 @@ import LoginTitleSection from "@/components/features/pages/LoginTitleSection";
 
 export default function LoginPage() {
   return (
-    <div className="pt-[120px] pb-[120px] pl-[22px] pr-[22px]">
-      <Flex
-        flexDirection="flex-col"
-        alignItems="items-center"
-        className="gap-[50px]"
-      >
-        <LoginTitleSection />
-        <LoginButtonSection />
-      </Flex>
-    </div>
+    <Flex
+      flexDirection="flex-col"
+      alignItems="items-center"
+      className="gap-[50px]"
+    >
+      <LoginTitleSection />
+      <LoginButtonSection />
+    </Flex>
   );
 }
