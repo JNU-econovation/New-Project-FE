1368f4f0c2c1af0fec981ded5a33650b6850f81a
minbo <smb5174@naver.com>
2025-04-12 14:32:33 +0900

    chore: section 태그 추가
    컴포넌트명에 맞게 section 태그를 추가하였습니다.

--- src/components/features/pages/LoginButtonSection/index.tsx
+++ src/components/features/pages/LoginButtonSection/index.tsx
@@ -7,37 +7,39 @@ import Text from "@/components/common/shared/ui/Text";
 
 export default function LoginButtonSection() {
   return (
-    <Flex flexDirection="flex-col" justifyContent="justify-normal" gap={4}>
-      <button className="pb-4 pt-4 bg-kakao-yellow rounded-lg">
-        <Flex
-          flexDirection="flex-row"
-          alignItems="items-center"
-          justifyContent="justify-center"
-          gap={3}
-        >
-          <Image src={Kakao_Logo} alt="카카오 로고" />
-          <Text fontWeight="font-semibold" fontSize="text-sm">
-            카카오톡으로 3초만에 시작하기
-          </Text>
-        </Flex>
-      </button>
-      <button className="pb-4 pt-4 bg-black rounded-lg">
-        <Flex
-          flexDirection="flex-row"
-          alignItems="items-center"
-          justifyContent="justify-center"
-          gap={3}
-        >
-          <Image src={Apple_Logo} alt="애플 로고" />
-          <Text
-            color="text-white"
-            fontSize="text-sm"
-            fontWeight="font-semibold"
+    <section className="w-full">
+      <Flex flexDirection="flex-col" justifyContent="justify-normal" gap={4}>
+        <button className="pb-4 pt-4 bg-kakao-yellow rounded-lg">
+          <Flex
+            flexDirection="flex-row"
+            alignItems="items-center"
+            justifyContent="justify-center"
+            gap={3}
           >
-            Apple로 계속하기
-          </Text>
-        </Flex>
-      </button>
-    </Flex>
+            <Image src={Kakao_Logo} alt="카카오 로고" />
+            <Text fontWeight="font-semibold" fontSize="text-sm">
+              카카오톡으로 3초만에 시작하기
+            </Text>
+          </Flex>
+        </button>
+        <button className="pb-4 pt-4 bg-black rounded-lg">
+          <Flex
+            flexDirection="flex-row"
+            alignItems="items-center"
+            justifyContent="justify-center"
+            gap={3}
+          >
+            <Image src={Apple_Logo} alt="애플 로고" />
+            <Text
+              color="text-white"
+              fontSize="text-sm"
+              fontWeight="font-semibold"
+            >
+              Apple로 계속하기
+            </Text>
+          </Flex>
+        </button>
+      </Flex>
+    </section>
   );
 }
