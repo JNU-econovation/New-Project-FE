07c6b82350a0adc84bf48ffc2835d8c70d5012a0
minbo <smb5174@naver.com>
2025-04-12 14:32:33 +0900

    chore: section 태그 추가
    컴포넌트명에 맞게 section 태그를 추가하였습니다.

--- src/components/features/pages/LoginTitleSection/index.tsx
+++ src/components/features/pages/LoginTitleSection/index.tsx
@@ -8,24 +8,26 @@ import Text from "@shared/ui/Text";
 
 export default function LoginTitleSection() {
   return (
-    <Flex flexDirection="flex-col" alignItems="items-center" gap={12}>
-      <Flex
-        flexDirection="flex-row"
-        alignItems="items-end"
-        gap={3}
-        maxWidth="max-w-fit"
-      >
-        <Flex flexDirection="flex-col" maxWidth="max-w-fit">
-          <Text fontWeight="font-bold" className="text-3xl w-fit">
-            그럼,
-          </Text>
-          <Text fontWeight="font-bold" className="text-3xl w-fit">
-            모험을 시작해볼까요?
-          </Text>
+    <section>
+      <Flex flexDirection="flex-col" alignItems="items-center" gap={12}>
+        <Flex
+          flexDirection="flex-row"
+          alignItems="items-end"
+          gap={3}
+          maxWidth="max-w-fit"
+        >
+          <Flex flexDirection="flex-col" maxWidth="max-w-fit">
+            <Text fontWeight="font-bold" className="text-3xl w-fit">
+              그럼,
+            </Text>
+            <Text fontWeight="font-bold" className="text-3xl w-fit">
+              모험을 시작해볼까요?
+            </Text>
+          </Flex>
+          <Image src={Star} alt="별" />
         </Flex>
-        <Image src={Star} alt="별" />
+        <Image src={Login_Logo} alt="로그인 페이지 로고" />
       </Flex>
-      <Image src={Login_Logo} alt="로그인 페이지 로고" />
-    </Flex>
+    </section>
   );
 }
