da56715c9a4bc825be0ba711d13b738575ac14b5
minbo <smb5174@naver.com>
2025-04-13 22:35:33 +0900

    chore: 폴더 구조 변경
    훅을 하나의 파일에서 작성하고 index.ts 파일에서 re-export 하는 방법으로
    변경하였습니다.

--- src/hooks/social-login-loading/index.ts
+++ src/hooks/social-login-loading/index.ts
@@ -1,30 +1,3 @@
-"use client";
+import useSaveToken from "@/hooks/social-login-loading/useSaveToken";
 
-import ROUTE from "@/constants/route";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-export const useSaveToken = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const accessToken = searchParams.get("accessToken");
-  const refreshToken = searchParams.get("refreshToken");
-
-  useEffect(() => {
-    if (accessToken && refreshToken) {
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-
-      const localStorageAccessToken = localStorage.getItem("accessToken");
-      const localStorageRefreshToken = localStorage.getItem("refreshToken");
-
-      if (
-        localStorageAccessToken === accessToken &&
-        localStorageRefreshToken === refreshToken
-      ) {
-        router.push(ROUTE.MAIN);
-      }
-    }
-  }, [accessToken, refreshToken, router]);
-};
+export { useSaveToken };
