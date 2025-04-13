f24a1e1165d9d1e73964be9b730bc6125fee0640
minbo <smb5174@naver.com>
2025-04-13 22:41:34 +0900

    fix: 빌드 에러 수정
    useSearchParams() 훅은 Suspense 컴포넌트안에서 호출되어야 하기 때문에
    수정하였습니다.

--- src/app/social-login-loading/page.tsx
+++ src/app/social-login-loading/page.tsx
@@ -1,5 +1,10 @@
 import SpinnerSection from "@/components/features/pages/social-login-loading/SpinnerSection";
+import { Suspense } from "react";
 
 export default function SocialLoginLoadingPage() {
-  return <SpinnerSection />;
+  return (
+    <Suspense>
+      <SpinnerSection />
+    </Suspense>
+  );
 }
