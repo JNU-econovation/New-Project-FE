7e8b0955c90e0ff1daf437f85f75409a1ed52a06
seunghyeon77 <jdyjsh77@naver.com>
2025-04-14 13:34:52 +0900

    refactor: className, children props 삭제

--- src/components/common/shared/layout/Spacing/index.tsx
+++ src/components/common/shared/layout/Spacing/index.tsx
@@ -1,11 +1,9 @@
 import { cn } from "@/utils/cn";
 
 interface SpacingProps {
-  children?: React.ReactNode;
-  className?: string;
   size: number;
 }
 
-export default function Spacing({ children, size, className }: SpacingProps) {
-  return <div className={cn(`mt-${size}`, className)}>{children}</div>;
+export default function Spacing({ size }: SpacingProps) {
+  return <div className={cn(`mt-${size}`)}></div>;
 }
