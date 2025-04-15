9b47c060508daf5c049fd62e943a9f84c3c8cdff
seunghyeon77 <jdyjsh77@naver.com>
2025-04-15 22:23:13 +0900

    refactor: 중복되는 Input을 TextField 컴포넌트로 변경

--- src/components/features/pages/EmailAuth/EmailAuthSection/index.tsx
+++ src/components/features/pages/EmailAuth/EmailAuthSection/index.tsx
@@ -1,7 +1,7 @@
 import Spacing from "@/components/common/shared/layout/Spacing";
 import Button from "@/components/common/shared/ui/Button";
-import Input from "@/components/common/shared/ui/Input";
 import Text from "@/components/common/shared/ui/Text";
+import TextField from "@/components/common/shared/ui/TextField";
 
 export default function EmailAuthSection() {
   return (
@@ -10,27 +10,13 @@ export default function EmailAuthSection() {
         메일 인증
       </Text>
 
-      <Spacing size={8} />
-
-      <>
-        <Text fontWeight="font-bold">이메일</Text>
-        <Spacing size={1} />
-        <div className="relative">
-          <Input
-            type="email"
-            placeholder="이메일"
-            color="primary"
-            size="primary"
-          />
-          <div className="absolute right-5 top-3">
-            <Button color="white" size="sm">
-              <Text fontWeight="font-semibold" className="font-semibold">
-                인증 요청
-              </Text>
-            </Button>
-          </div>
+      <TextField label="이메일" type="email" placeholder="이메일">
+        <div className="absolute right-5 top-2">
+          <Button color="white">
+            <Text fontWeight="font-semibold">인증 요청</Text>
+          </Button>
         </div>
-      </>
+      </TextField>
 
       <Text
         fontSize="text-sm"
@@ -40,26 +26,18 @@ export default function EmailAuthSection() {
         존재하지 않는 이메일입니다.
       </Text>
 
-      <Spacing size={6} />
+      <Spacing size={8} />
 
-      <>
-        <Text fontWeight="font-bold">인증번호</Text>
-        <Spacing size={1} />
-        <div className="relative">
-          <Input
-            type="number"
-            placeholder="인증번호"
-            color="primary"
-            size="primary"
-          />
-          <span className="absolute right-7 top-4">04:59</span>
-        </div>
-      </>
+      <TextField label="인증번호" type="number" placeholder="인증번호">
+        <span className="absolute right-7 top-4">04:59</span>
+      </TextField>
 
       <Spacing size={8} />
 
-      <Button color="black" size="lg">
-        확인하기
+      <Button color="black">
+        <Text fontWeight="font-bold" color="text-white">
+          확인하기
+        </Text>
       </Button>
     </>
   );
