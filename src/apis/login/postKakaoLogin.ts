1e959930f0f9ab5005129bbb10e06518e887beef
minbo <smb5174@naver.com>
2025-04-30 16:36:40 +0900

    refactor: 요청 api uri와 mutationKey를 상수 데이터로 관리

--- src/apis/login/postKakaoLogin.ts
+++ src/apis/login/postKakaoLogin.ts
@@ -2,8 +2,10 @@ import { instance } from "@/apis/instances";
 
 // Todo: 카카오 로그인 API 경로 변경
 
+export const KAKAO_LOGIN_URI = `${process.env.NEXT_PUBLIC_BASE_URL}auth/users/signup`;
+
 const postKakaoLogin = async () => {
-  await instance.post("auth/users/signup", {});
+  await instance.post(KAKAO_LOGIN_URI, {});
 };
 
 export default postKakaoLogin;
