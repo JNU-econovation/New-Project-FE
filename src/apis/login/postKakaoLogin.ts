2c5a1fb07ebe857a8d11636304c68bcb73cc4e5b
minbo <smb5174@naver.com>
2025-04-30 16:17:44 +0900

    chore: 변수명 변경
    axiosInstance에서 instance로 변수명을 변경하였습니다.

--- src/apis/login/postKakaoLogin.ts
+++ src/apis/login/postKakaoLogin.ts
@@ -1,9 +1,9 @@
-import { axiosInstance } from "@/apis/instances";
+import { instance } from "@/apis/instances";
 
 // Todo: 카카오 로그인 API 경로 변경
 
 const postKakaoLogin = async () => {
-  await axiosInstance.post("auth/users/signup", {});
+  await instance.post("auth/users/signup", {});
 };
 
 export default postKakaoLogin;
