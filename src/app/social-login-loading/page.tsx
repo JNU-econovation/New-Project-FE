cacfbca79ffb28f8f6d93b26baee1204dd7f8819
minbo <smb5174@naver.com>
2025-05-03 18:16:29 +0900

    feat: 소셜 로그인 로딩 페이지 구현
    소셜 로그인 후 이를 처리할 페이지를 구현하였습니다.

---
+++ src/app/social-login-loading/page.tsx
@@ -0,0 +1,10 @@
+import SpinnerSection from "@/components/features/pages/social-login-loading/SpinnerSection";
+import { Suspense } from "react";
+
+export default function SocialLoginLoadingPage() {
+  return (
+    <Suspense>
+      <SpinnerSection />
+    </Suspense>
+  );
+}
