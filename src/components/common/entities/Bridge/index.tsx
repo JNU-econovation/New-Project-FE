be41adc20d2ff9d5ec0a6e0ab1052e510c43ebe7
박건규 <geongyu09@gmail.com>
2025-05-07 09:48:48 +0900

    feat: 웹뷰가 통신을 할 수 있는 상태인지를 확인할 수 있는 3-way-handshake 로직 추가
    웹뷰가 통신을 할 수 있는 상태가 된 시점을 알기 위해서 http 통신의 3way-handshake 형식으로 요청을 보내는 절차를 추가합니다.
    
    #10

--- src/components/common/entities/Bridge/index.tsx
+++ src/components/common/entities/Bridge/index.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 declare global {
   interface Window {
+    ReactNativeWebView?: {
+      postMessage(message: string): void;
+    };
+  }
+  interface Document {
     ReactNativeWebView?: {
       postMessage(message: string): void;
     };
@@ -26,19 +31,82 @@ interface BridgeProps {
   onRequest: (reqMessage: MessageEventRequestData) => MessageEventResponseData;
 }
 
+type Flag = 0 | 1;
+
+interface WebviewHandshake {
+  name: "webview-handshake";
+  flag: {
+    syn: Flag;
+    ack: Flag;
+  };
+}
+
+const TIMEOUT = 1000;
+
+function postMessage<Data>(
+  message: MessageEventResponseData<Data> | WebviewHandshake
+) {
+  window.ReactNativeWebView?.postMessage(JSON.stringify(message));
+  document.ReactNativeWebView?.postMessage(JSON.stringify(message));
+}
+
 const Bridge = ({ onRequest }: BridgeProps) => {
-  useLayoutEffect(() => {
+  useEffect(() => {
     const handleMessage = ({ data }: MessageEvent) => {
-      const requestMessage = JSON.parse(data) as MessageEventRequestData;
-      const responseMessage = onRequest(requestMessage);
-      if (!responseMessage) return;
-      window.ReactNativeWebView?.postMessage(JSON.stringify(responseMessage));
+      try {
+        const requestMessage =
+          typeof data === "string" ? JSON.parse(data) : data;
+
+        if (requestMessage && requestMessage.method && requestMessage.name) {
+          const responseMessage = onRequest(
+            requestMessage as MessageEventRequestData
+          );
+          postMessage(responseMessage);
+        }
+      } catch (error) {
+        console.error("메시지 처리 중 오류 발생:", error);
+      }
     };
 
     window.addEventListener("message", handleMessage);
 
     return () => window.removeEventListener("message", handleMessage);
   }, [onRequest]);
+
+  useEffect(() => {
+    const webviewReady = (syn: Flag, ack: Flag) => {
+      postMessage({ name: "webview-handshake", flag: { syn, ack } });
+      postMessage({ name: "webview-handshake", flag: { syn, ack } });
+    };
+    webviewReady(1, 0);
+
+    const timeoutId = setTimeout(() => {
+      webviewReady(1, 0);
+    }, TIMEOUT);
+
+    const handleMessage = (event: MessageEvent) => {
+      event.stopPropagation();
+      try {
+        const {
+          name,
+          flag: { syn, ack },
+        } =
+          typeof event.data === "string" ? JSON.parse(event.data) : event.data;
+
+        if (name === "webview-handshake" && syn === 1 && ack === 1) {
+          webviewReady(0, 1);
+
+          if (timeoutId) clearTimeout(timeoutId);
+
+          window.removeEventListener("message", handleMessage);
+        }
+      } catch (error) {
+        console.error("handshake 중 에러 발생:", error);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+  }, []);
 
   return null;
 };
