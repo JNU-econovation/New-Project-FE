2167258d07cd2253c259f691a6cfd9c8eb77a725
박건규 <geongyu09@gmail.com>
2025-05-16 16:28:29 +0900

    feat: 로그인 페이지에서 사용되는 브리지 로직이 있는 컴포넌트 구현
    해당 컴포넌트에서 올바르게 카카오 로그인 요청을 한 경우 카카오 로그인을 진행하도록 브리지 로직 추가
    
    #18

---
+++ src/components/features/widgets/LoginBridge/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Bridge from "@/components/common/entities/Bridge";
+import { useKakaoLoginMutate } from "@/hooks/query/login/useKakaoLoginMutate";
+import {
+  MessageEventRequestData,
+  MessageEventResponseData,
+} from "@/type/bridge";
+
+export default function LoginBridge() {
+  const { mutate: kakaoLogin } = useKakaoLoginMutate();
+
+  const onRequest = (
+    requestMessage: MessageEventRequestData
+  ): MessageEventResponseData => {
+    const { method, name } = requestMessage;
+
+    if (method !== "POST")
+      return { status: "error", name, data: "올바른 메서드가 아닙니다" };
+
+    if (name === "request-kakao") {
+      kakaoLogin();
+      return { status: "success", name };
+    }
+
+    return {
+      status: "error",
+      name,
+      data: "올바른 요청이 아닙니다",
+    };
+  };
+
+  return <Bridge onRequest={onRequest} />;
+}
