e6d85224cb6fce3a0abed4a85e1ff5a2cea27284
박건규 <geongyu09@gmail.com>
2025-05-16 16:30:18 +0900

    feat: 토큰을 앱으로 전달하는 useSendToken 훅 구현
    #18

---
+++ src/hooks/useSendToken/index.ts
@@ -0,0 +1,29 @@
+import { useBridge } from "@hooks/useBridge";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const useSendToken = () => {
+  const searchParams = useSearchParams();
+  const { request } = useBridge();
+
+  const accessToken = searchParams.get("access_token");
+  const refreshToken = searchParams.get("refresh_token");
+
+  useEffect(() => {
+    if (!accessToken && !refreshToken) {
+      request({
+        requestMessage: {
+          name: "put-token",
+          method: "POST",
+          body: {
+            accessToken,
+            refreshToken,
+          },
+        },
+        responseCallback: () => {},
+      });
+    }
+  }, [accessToken, refreshToken, request]);
+};
+
+export default useSendToken;
