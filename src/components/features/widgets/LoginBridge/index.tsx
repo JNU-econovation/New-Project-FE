7f3efc46c557421fde1e2f9a41930af7cec67b47
박건규 <geongyu09@gmail.com>
2025-05-17 20:22:53 +0900

    refactor: LoginBridge에서 onRequest 콜백 함수 useCallback으로 메모이제이션
    #18

--- src/components/features/widgets/LoginBridge/index.tsx
+++ src/components/features/widgets/LoginBridge/index.tsx
@@ -6,29 +6,31 @@ import {
   MessageEventRequestData,
   MessageEventResponseData,
 } from "@/type/bridge";
+import { useCallback } from "react";
 
 export default function LoginBridge() {
   const { mutate: kakaoLogin } = useKakaoLoginMutate();
 
-  const onRequest = (
-    requestMessage: MessageEventRequestData
-  ): MessageEventResponseData => {
-    const { method, name } = requestMessage;
+  const onRequest = useCallback(
+    (requestMessage: MessageEventRequestData): MessageEventResponseData => {
+      const { method, name } = requestMessage;
 
-    if (method !== "POST")
-      return { status: "error", name, data: "올바른 메서드가 아닙니다" };
+      if (method !== "POST")
+        return { status: "error", name, data: "올바른 메서드가 아닙니다" };
 
-    if (name === "request-kakao") {
-      kakaoLogin();
-      return { status: "success", name };
-    }
+      if (name === "request-kakao") {
+        kakaoLogin();
+        return { status: "success", name };
+      }
 
-    return {
-      status: "error",
-      name,
-      data: "올바른 요청이 아닙니다",
-    };
-  };
+      return {
+        status: "error",
+        name,
+        data: "올바른 요청이 아닙니다",
+      };
+    },
+    [kakaoLogin]
+  );
 
   return <Bridge onRequest={onRequest} />;
 }
