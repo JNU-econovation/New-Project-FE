97f724fab6c235e1662afc532dc3857def8b31c8
박건규 <geongyu09@gmail.com>
2025-05-19 10:44:33 +0900

    feat: 산행 검색 페이지에서 산 그리드를 클릭했을 때 해당 산의 코스 검색 페이지로 이동하도록 브리지 로직 추가
    #20

--- src/components/features/pages/course/MountainGridSection/index.tsx
+++ src/components/features/pages/course/MountainGridSection/index.tsx
@@ -1,17 +1,39 @@
+"use client";
+
+import { useBridge } from "@/hooks/common/useBridge";
 import Spacing from "@shared/layout/Spacing";
 import Text from "@shared/ui/Text";
+import { useCallback } from "react";
 
 export default function MountainGridSection() {
+  const { request } = useBridge();
+
+  const postNavigateMessage = useCallback(
+    (mountainName: string) => {
+      request({
+        requestMessage: {
+          method: "POST",
+          name: "request-navigate",
+          body: {
+            mountainName,
+          },
+        },
+      });
+    },
+    [request]
+  );
+
   return (
     <section className="flex grow gap-4">
       <div className="flex flex-col w-full h-fit gap-4">
-        <div
+        <button
           className="h-72 rounded-2xl flex items-center justify-center relative overflow-hidden"
           style={{
             backgroundImage: `url('/images/mudeungsan.jpg')`,
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
           }}
+          onClick={() => postNavigateMessage("무등산")}
         >
           <div className="absolute top-0 right-0 bg-black opacity-40 w-full h-full" />
           <div className="absolute top-6 right-4">
@@ -32,7 +54,7 @@ export default function MountainGridSection() {
           >
             무등산
           </Text>
-        </div>
+        </button>
 
         {/* TODO: 실제 산으로 변경하기 */}
         <div className="h-72 bg-amber-100 rounded-2xl" />
