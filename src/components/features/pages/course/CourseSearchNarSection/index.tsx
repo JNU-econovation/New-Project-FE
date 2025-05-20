dbe4390b8c2de2b18bfc49755e9362fcde36a04f
박건규 <geongyu09@gmail.com>
2025-05-20 23:20:36 +0900

    feat: 코스 검색 페이지의 input section 구현
    #20

---
+++ src/components/features/pages/course/CourseSearchNarSection/index.tsx
@@ -0,0 +1,9 @@
+import SearchInput from "@shared/ui/SearchInput";
+
+export default function CourseSearchBarSection() {
+  return (
+    <section>
+      <SearchInput placeholder="코스 지점 직접 검색" />
+    </section>
+  );
+}
