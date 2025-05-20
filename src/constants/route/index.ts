a1f607f6a2327895bedb5c9509fb244ec2d829f7
박건규 <geongyu09@gmail.com>
2025-05-20 23:22:31 +0900

    chore:  추가된 페이지에 맞추어 route 전역변수 업데이트
    COURSE, COURSE_SEARCH_RESULT 를 추가하였습니다
    여기서 id값 처리를 위하여 함수형테로 COURSE_SEARCH_RESULT를 추가하였습니다
    
    #20

--- src/constants/route/index.ts
+++ src/constants/route/index.ts
@@ -2,6 +2,14 @@ const ROUTE = {
   MAIN: "/",
   LOGIN: "/login",
   SOCIAL_LOGIN_LOADING: "/social-login-loading",
+  COURSE: "/course",
+  COURSE_SEARCH_RESULT: ({
+    mountainName,
+    sort = "my",
+  }: {
+    mountainName: string;
+    sort?: string;
+  }) => `/course/${mountainName}?sort=${sort}`,
 };
 
 export default ROUTE;
