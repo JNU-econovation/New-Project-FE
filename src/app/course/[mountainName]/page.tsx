16a3117b42644cbffcaa830d043b8b193a92f58c
박건규 <geongyu09@gmail.com>
2025-05-20 23:30:53 +0900

    feat: mountainCoursePage 구현
    #20

--- src/app/course/[mountainName]/page.tsx
+++ src/app/course/[mountainName]/page.tsx
@@ -1,3 +1,18 @@
+import CourseSearchBarSection from "@pages/course/CourseSearchNarSection";
+import CourseTabSection from "@pages/course/CourseTabBarSection";
+import Spacing from "@shared/layout/Spacing";
+import BackButton from "@/components/features/widgets/BackButton";
+
 export default function MountainCoursePage() {
-  return <div>MountainCoursePage</div>;
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      <div className="px-6 pt-12 flex flex-col">
+        <BackButton />
+        <Spacing size={8} />
+        <CourseSearchBarSection />
+        <Spacing size={4} />
+      </div>
+      <CourseTabSection />
+    </div>
+  );
 }
