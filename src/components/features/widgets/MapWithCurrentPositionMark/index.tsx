8065bbeecc882723304935df30a599c2962af1c7
박건규 <geongyu09@gmail.com>
2025-05-21 20:50:01 +0900

    feat: 현재 위치 마크가 있는 지도 공통 컴포넌트 구현
    #24

---
+++ src/components/features/widgets/MapWithCurrentPositionMark/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import useGetCurrentPositionBridge from "@/hooks/bridge/useGetCurrentPositionBridge";
+import { Position } from "@/hooks/bridge/useGetCurrentPositionBridge";
+import useLogBridge from "@/hooks/bridge/useLogBridge";
+import { useNaverMap } from "@/hooks/feature/map/useNaverMap";
+import { useSetMarker } from "@/hooks/feature/map/useSetMarker";
+import { useState } from "react";
+
+const defaultPosition = { latitude: 35.9789064, longitude: 126.9079232 };
+
+export default function MapWithCurrentPositionMark() {
+  // const { currentPosition } = useGetCurrentPosition();
+  const [currentPosition, setCurrentPosition] = useState<{
+    latitude: number;
+    longitude: number;
+  }>(defaultPosition);
+
+  const getCurrentPosition = useGetCurrentPositionBridge();
+
+  const onResponse = ({ coords }: Position) => {
+    const { latitude, longitude } = coords;
+    setCurrentPosition({ latitude, longitude });
+  };
+
+  getCurrentPosition(onResponse);
+
+  const { mapId, map } = useNaverMap(currentPosition);
+  const logBridge = useLogBridge();
+
+  useSetMarker(map, currentPosition ?? defaultPosition);
+  logBridge(currentPosition);
+
+  return <div id={mapId} className="h-screen w-screen" />;
+}
