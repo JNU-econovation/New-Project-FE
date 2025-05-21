3178028203d1f40b94bf39351263519ee4179b3e
박건규 <geongyu09@gmail.com>
2025-05-21 20:47:42 +0900

    feat: 지도에 경로를 그리는 훅 구현
    #24

---
+++ src/hooks/feature/map/useDrawPath/index.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+type LatLongPath = [number, number][];
+
+// 경로 표시 : [위도, 경도] 의 배열
+export const useDrawPath = (map: any, path: LatLongPath) => {
+  useEffect(() => {
+    (async () => {
+      // console.log(res.route.traoptimal[0].path);
+
+      new naver.maps.Polyline({
+        map,
+        path,
+        strokeWeight: 5,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeStyle: "solid",
+        strokeLineCap: "round",
+        strokeLineJoin: "round",
+      });
+    })();
+  }, [map]);
+};
