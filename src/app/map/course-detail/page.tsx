a1904a727e2de14989dac287850f3b54a6739b6e
박건규 <geongyu09@gmail.com>
2025-05-24 11:09:40 +0900

    feat: 변경된 section 컴포넌트에 맞추어 코스 상세 페이지 구현
    #24

--- src/app/map/course-detail/page.tsx
+++ src/app/map/course-detail/page.tsx
@@ -1,11 +1,33 @@
-import CourseDetailMapSection from "@/components/features/pages/map/CourseDetailMapSection";
-
-// const ACCESS_KEY = process.env.NEXT_PUBLIC_NAVER_KEY;
+import CourseDetailMapTagHeaderSection from "@pages/map/CourseDetailMapTagHeaderSection";
+import TravelStartButton from "@widgets/TravelStartButton";
+import CourseDetailBottomSheetSection from "@pages/map/CourseDetailBottomSheetSection";
+import CourseDetailMapSection from "@pages/map/CourseDetailMapSection";
+import PositionBottom from "@shared/layout/PositionBottom";
+import Spacing from "@shared/layout/Spacing";
+import BackButton from "@widgets/BackButton";
 
 export default function CourseDetailPage() {
   return (
-    <>
+    <div className="relative w-screen h-screen">
+      <div className="px-6 z-10 fixed">
+        <Spacing size={4} />
+        <BackButton />
+        <Spacing size={3} />
+        <CourseDetailMapTagHeaderSection />
+      </div>
+      <PositionBottom bottom={72}>
+        <div className="px-6">
+          <CourseDetailBottomSheetSection />
+        </div>
+      </PositionBottom>
+
+      <PositionBottom>
+        <div className="px-6">
+          <TravelStartButton />
+        </div>
+      </PositionBottom>
+      {/* </div> */}
       <CourseDetailMapSection />
-    </>
+    </div>
   );
 }
