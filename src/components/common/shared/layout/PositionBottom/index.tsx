f5b1223bf43f84738d9b9eecb415e59fb1f7fefe
박건규 <geongyu09@gmail.com>
2025-05-25 23:48:14 +0900

    feat: PositionBottom 컴포넌트에 z-index 추가
    #24

--- src/components/common/shared/layout/PositionBottom/index.tsx
+++ src/components/common/shared/layout/PositionBottom/index.tsx
@@ -5,6 +5,7 @@ const DEFAULT_BOTTOM_SPACE = 18; // 하단 여백
 
 interface PositionBottomProps extends PropsWithChildren {
   bottom?: number;
+  zIndex?: number;
 }
 
 export default function PositionBottom({
@@ -12,7 +13,10 @@ export default function PositionBottom({
   bottom = DEFAULT_BOTTOM_SPACE,
 }: PositionBottomProps) {
   return (
-    <div className={cn("absolute left-0 w-full")} style={{ bottom }}>
+    <div
+      className={cn("absolute left-0 w-full")}
+      style={{ bottom, zIndex: 10 }}
+    >
       {children}
     </div>
   );
