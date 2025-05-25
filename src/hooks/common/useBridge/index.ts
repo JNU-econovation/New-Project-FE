a4a072a887a4e4d5f3f3ac3db828c02a9e456ff1
박건규 <geongyu09@gmail.com>
2025-05-25 23:45:20 +0900

    chore: 코스 상세 페이지에서 바텀 시트의 위치 조정
    #24

--- src/hooks/common/useBridge/index.ts
+++ src/hooks/common/useBridge/index.ts
@@ -11,6 +11,7 @@ type EventHandler = (event: MessageEvent) => void;
 interface RequestProps<ReqBody = unknown, ResBody = unknown> {
   requestMessage: MessageEventRequestData<ReqBody>;
   responseCallback?: (resMessage: MessageEventResponseData<ResBody>) => void;
+  onErrorCallback?: (error: Error) => void;
 }
 
 // key : bridge name
@@ -32,33 +33,45 @@ export const useBridge = () => {
   const request = <ReqBody = unknown, ResBody = unknown>({
     requestMessage,
     responseCallback,
+    onErrorCallback,
   }: RequestProps<ReqBody, ResBody>) => {
-    if (typeof window === "undefined") return;
+    try {
+      if (typeof window === "undefined") return;
 
-    window.ReactNativeWebView?.postMessage(JSON.stringify(requestMessage));
-    document.ReactNativeWebView?.postMessage(JSON.stringify(requestMessage));
+      window.ReactNativeWebView?.postMessage(JSON.stringify(requestMessage));
+      document.ReactNativeWebView?.postMessage(JSON.stringify(requestMessage));
 
-    const handler = (event: MessageEvent) => {
-      const rsvMessage = JSON.parse(
-        event.data
-      ) as MessageEventResponseData<ResBody>;
-      RcvBuffer.map((item) => Object.keys(item)[0]).forEach((name, index) => {
-        if (name === rsvMessage.name) {
-          window.removeEventListener("message", RcvBuffer[index][name]);
-          RcvBuffer.splice(index, 1);
+      const handler = (event: MessageEvent) => {
+        try {
+          const rsvMessage = JSON.parse(
+            event.data
+          ) as MessageEventResponseData<ResBody>;
+          RcvBuffer.map((item) => Object.keys(item)[0]).forEach(
+            (name, index) => {
+              if (name === rsvMessage.name) {
+                window.removeEventListener("message", RcvBuffer[index][name]);
+                RcvBuffer.splice(index, 1);
 
-          if (!responseCallback) return;
-          responseCallback(rsvMessage);
+                if (!responseCallback) return;
+                responseCallback(rsvMessage);
+              }
+            }
+          );
+        } catch (error) {
+          if (onErrorCallback) return onErrorCallback(error as Error);
+          console.error("Error in useBridge response", error);
+          console.error("Error in useBridge response", event.data);
         }
+      };
+
+      RcvBuffer.push({
+        [requestMessage.name]: handler,
       });
-    };
 
-    // eventHandlers.push(handler);
-    RcvBuffer.push({
-      [requestMessage.name]: handler,
-    });
-
-    window.addEventListener("message", handler);
+      window.addEventListener("message", handler);
+    } catch (error) {
+      console.error("Error in useBridge request", error);
+    }
   };
 
   return { request };
