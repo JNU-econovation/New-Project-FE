fda49554ea2284c5ec1cff34f85f78e994754c8a
박건규 <geongyu09@gmail.com>
2025-05-25 16:27:11 +0900

    refactor: 코스 상세 페이지의 bottom sheet에 캐러셀 적용
    #24

--- src/components/features/pages/map/CourseDetailBottomSheetSection/index.tsx
+++ src/components/features/pages/map/CourseDetailBottomSheetSection/index.tsx
@@ -1,35 +1,31 @@
+"use client";
+
+import BottomSheet from "@entities/BottomSheet";
+import Carousel from "@entities/Carousel";
+import CourseBaseTab from "@entities/CourseBaseTab";
+import CourseImageInfoSection from "@pages/map/CourseImageInfoSection";
+import CourseWeatherClothesInfoSection from "@pages/map/CourseWeatherClothesInfoSection";
 import Spacing from "@shared/layout/Spacing";
-import BottomSheet from "@shared/ui/BottomSheet";
+import CourseMetaDataUi from "@shared/ui/CourseMetaDataUi";
 
 export default function CourseDetailBottomSheetSection() {
+  const courseList = ["증심사", "중머리재", "원효분소"];
+
   return (
     <section>
       <BottomSheet>
-        <div className="flex items-center justify-center">
-          <span className="border px-4 py-1 rounded-2xl">증심사</span>
-          <span className="border px-4 py-1 rounded-2xl">증심사</span>
-          <span className="border px-4 py-1 rounded-2xl">증심사</span>
-        </div>
+        <CourseBaseTab courseList={courseList} />
+        <Spacing size={4} />
+        <CourseMetaDataUi difficulty="easy" distance={123} time={23} />
         <Spacing size={4} />
 
-        <div className="flex items-center justify-between">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <span>icon</span>
-              <span>41km</span>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <span>icon</span>
-              <span>41km</span>
-            </div>
-          </div>
-        </div>
-        <Spacing size={4} />
-        <div className="h-36 bg-green-200" />
+        <Carousel
+          items={[
+            <CourseWeatherClothesInfoSection key={1} />,
+            <CourseImageInfoSection key={2} />,
+          ]}
+        />
       </BottomSheet>
-      <Spacing size={4} />
     </section>
   );
 }
