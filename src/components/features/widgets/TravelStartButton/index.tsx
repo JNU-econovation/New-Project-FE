8fd00014640e934fcc9d1e557d6001fa28370d3c
박건규 <geongyu09@gmail.com>
2025-05-26 10:29:49 +0900

    feat: 산행 시작 버튼 기능 추가
    #24

--- src/components/features/widgets/TravelStartButton/index.tsx
+++ src/components/features/widgets/TravelStartButton/index.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import useStartTravelBridge from "@hooks/bridge/useStartTravelBridge";
 import Button from "@shared/ui/Button";
 
 export default function TravelStartButton() {
-  return <Button fullWidth>산행 시작</Button>;
+  const startTravel = useStartTravelBridge();
+
+  return (
+    <Button fullWidth onClick={startTravel}>
+      산행 시작
+    </Button>
+  );
 }
