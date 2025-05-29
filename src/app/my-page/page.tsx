48ded845beb898275a8d9945620ee8b5dc3d5634
minbo <smb5174@naver.com>
2025-05-29 17:11:38 +0900

    feat: MyInfoSection 컴포넌트 구현

---
+++ src/app/my-page/page.tsx
@@ -0,0 +1,9 @@
+import MyInfoSection from "@/components/features/pages/my-page/MyInfoSection";
+
+export default function MyPage() {
+  return (
+    <>
+      <MyInfoSection />
+    </>
+  );
+}
--- src/constants/route/index.ts
+++ src/constants/route/index.ts
@@ -10,6 +10,10 @@ const ROUTE = {
     mountainName: string;
     sort?: string;
   }) => `/course/${mountainName}?sort=${sort}`,
+  MY_PAGE: "/my-page",
+  MY_INFO: "/my-page/my-info",
+  HIKING_LOG: "/my-page/hiking-log",
+  COURSE_BOOKMARK: "/my-page/course-bookmark",
 };
 
 export default ROUTE;
