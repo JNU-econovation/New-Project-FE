a05ad530929a68879b8f76f334a8af593d42e22b
minbo <smb5174@naver.com>
2025-05-29 17:11:38 +0900

    feat: MyInfoSection 컴포넌트 구현

---
+++ src/components/features/pages/my-page/MyInfoSection/index.tsx
@@ -0,0 +1,56 @@
+import Text from "@/components/common/shared/ui/Text";
+import ROUTE from "@/constants/route";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function MyInfoSection() {
+  return (
+    <div className="p-5">
+      <div className="w-full border-b border-gray-30 flex flex-col items-center">
+        <Text fontSize="text-2xl" className="pb-3">
+          마이 페이지
+        </Text>
+      </div>
+      <div className="flex flex-col items-center mt-5 gap-4">
+        <Image
+          src="/images/profile.png"
+          alt="나의 프로필"
+          width={100}
+          height={100}
+          className="rounded-full border border-gray-30"
+        />
+        <Link href={ROUTE.MY_INFO} className="flex flex-row items-center gap-2">
+          <Text fontSize="text-3xl" fontWeight="font-bold">
+            {"사용자 이름"}
+          </Text>
+          <Text fontSize="text-xl" fontWeight="font-bold">
+            {">"}
+          </Text>
+        </Link>
+        <Text fontSize="text-xl" color="text-main-green">
+          {"test@naver.com"}
+        </Text>
+        <div className="flex flex-row items-center gap-10">
+          <Link href={ROUTE.HIKING_LOG}>
+            <Text
+              fontSize="text-xl"
+              fontWeight="font-semibold"
+              color="text-main-green"
+            >
+              산행 기록
+            </Text>
+          </Link>
+          <Link href={ROUTE.COURSE_BOOKMARK}>
+            <Text
+              fontSize="text-xl"
+              fontWeight="font-semibold"
+              color="text-main-green"
+            >
+              코스 북마크
+            </Text>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
