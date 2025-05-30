dd743d9f9772be3a22e184bb960b6f07761d23d3
minbo <smb5174@naver.com>
2025-05-30 12:30:18 +0900

    feat: 고객 센터 관련 컴포넌트 구현

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,9 +1,14 @@
+import CustomerSection from "@/components/features/pages/my-page/CustomerCenterSection";
 import MyInfoSection from "@/components/features/pages/my-page/MyInfoSection";
 
 export default function MyPage() {
   return (
     <>
       <MyInfoSection />
+      <div className="h-3 bg-gray-100" />
+      <div className="p-5">
+        <CustomerSection />
+      </div>
     </>
   );
 }
---
+++ src/components/features/pages/my-page/CustomerCenterSection/index.tsx
@@ -0,0 +1,46 @@
+import Spacing from "@/components/common/shared/layout/Spacing";
+import Text from "@/components/common/shared/ui/Text";
+import ROUTE from "@/constants/route";
+import Link from "next/link";
+
+export default function CustomerSection() {
+  return (
+    <div className="border-b border-gray-30">
+      <Text fontSize="text-xl" color="text-main-green">
+        고객 센터
+      </Text>
+      <Spacing size={2} />
+      <Link href={ROUTE.FAQ} className="flex items-center justify-between h-10">
+        <Text fontSize="text-xl" fontWeight="font-medium">
+          자주 묻는 질문
+        </Text>
+        <Text fontSize="text-xl" fontWeight="font-medium" color="text-sub-gray">
+          {">"}
+        </Text>
+      </Link>
+      <Link
+        href={ROUTE.INQUIRY}
+        className="flex items-center justify-between h-10"
+      >
+        <Text fontSize="text-xl" fontWeight="font-medium">
+          문의하기
+        </Text>
+        <Text fontSize="text-xl" fontWeight="font-medium" color="text-sub-gray">
+          {">"}
+        </Text>
+      </Link>
+      <Link
+        href={ROUTE.INQUIRY_HISTORY}
+        className="flex items-center justify-between h-10"
+      >
+        <Text fontSize="text-xl" fontWeight="font-medium">
+          약관 확인
+        </Text>
+        <Text fontSize="text-xl" fontWeight="font-medium" color="text-sub-gray">
+          {">"}
+        </Text>
+      </Link>
+      <Spacing size={2} />
+    </div>
+  );
+}
