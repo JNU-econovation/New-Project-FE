e0dd71eb606d4d7b9192dd6ce0338b226f73888a
minbo <smb5174@naver.com>
2025-05-31 10:56:59 +0900

    feat: 위급 시 개인 정보 컴포넌트 구현

---
+++ src/components/features/pages/my-info/EmergencyPersonalInfoSection/index.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import Spacing from "@/components/common/shared/layout/Spacing";
+import Text from "@/components/common/shared/ui/Text";
+import TextField from "@/components/common/shared/ui/TextField";
+import Image from "next/image";
+import Cancel from "@/icons/Cancel.svg";
+import { useEmergencyPersonalInfo } from "@/hooks/feature/info/useEmergencyPersonalInfo";
+import { useModal } from "@/hooks/common/useModal";
+import WithdrawMemberModal from "@/components/common/entities/WithdrawMemberModal";
+
+export default function EmergencyPersonalInfoSection() {
+  const { emergencyPersonalInfo, onChangePersonalInfo, onClearPersonalInfo } =
+    useEmergencyPersonalInfo();
+
+  const { closeModal, isOpen, openModal } = useModal();
+
+  return (
+    <>
+      <Text
+        fontSize="text-xl"
+        fontWeight="font-semibold"
+        color="text-main-green"
+      >
+        위급 시 개인 정보
+      </Text>
+      <Spacing size={8} />
+      <TextField
+        label="몸무게"
+        placeholder="100"
+        color="white"
+        value={emergencyPersonalInfo.weight}
+        onChange={(e) => onChangePersonalInfo("weight", e.target.value)}
+        right={
+          <div className="flex items-center gap-2">
+            <Text fontSize="text-2xl" color="text-sub-gray">
+              kg
+            </Text>
+            <button onClick={() => onClearPersonalInfo("weight")}>
+              <Image src={Cancel} alt="입력 취소" width={24} height={24} />
+            </button>
+          </div>
+        }
+      />
+      <Spacing size={8} />
+      <TextField
+        label="키"
+        placeholder="100"
+        color="white"
+        value={emergencyPersonalInfo.height}
+        onChange={(e) => onChangePersonalInfo("height", e.target.value)}
+        right={
+          <div className="flex items-center gap-2">
+            <Text fontSize="text-2xl" color="text-sub-gray">
+              cm
+            </Text>
+            <button onClick={() => onClearPersonalInfo("height")}>
+              <Image src={Cancel} alt="입력 취소" width={24} height={24} />
+            </button>
+          </div>
+        }
+      />
+      <Spacing size={8} />
+      <TextField
+        label="혈액형"
+        placeholder="B"
+        color="white"
+        value={emergencyPersonalInfo.bloodType}
+        onChange={(e) => onChangePersonalInfo("bloodType", e.target.value)}
+        right={
+          <div className="flex items-center gap-2">
+            <Text fontSize="text-2xl" color="text-sub-gray">
+              형
+            </Text>
+            <button onClick={() => onClearPersonalInfo("bloodType")}>
+              <Image src={Cancel} alt="입력 취소" width={24} height={24} />
+            </button>
+          </div>
+        }
+      />
+      <Spacing size={8} />
+      <button onClick={openModal} className="flex justify-center w-full">
+        <Text className="text-center" color="text-sub-gray">
+          회원 탈퇴
+        </Text>
+      </button>
+      {isOpen && (
+        <WithdrawMemberModal closeModal={closeModal} openModal={openModal} />
+      )}
+    </>
+  );
+}
---
+++ src/hooks/feature/info/useEmergencyPersonalInfo/index.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const useEmergencyPersonalInfo = () => {
+  const [emergencyPersonalInfo, setEmergencyPersonalInfo] = useState({
+    weight: "",
+    height: "",
+    bloodType: "",
+  });
+
+  const onChangePersonalInfo = (key: string, value: string) => {
+    setEmergencyPersonalInfo((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  const onClearPersonalInfo = (key: keyof typeof emergencyPersonalInfo) => {
+    setEmergencyPersonalInfo((prev) => ({
+      ...prev,
+      [key]: "",
+    }));
+  };
+
+  return {
+    emergencyPersonalInfo,
+    onChangePersonalInfo,
+    onClearPersonalInfo,
+  };
+};
