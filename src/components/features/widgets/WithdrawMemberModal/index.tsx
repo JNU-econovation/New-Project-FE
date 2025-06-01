9bc9ad4c344a24e55c766b8aca018ae08e7a2278
minbo <smb5174@naver.com>
2025-06-01 12:43:17 +0900

    chore: alias 적용

--- src/components/features/widgets/WithdrawMemberModal/index.tsx
+++ src/components/features/widgets/WithdrawMemberModal/index.tsx
@@ -1,10 +1,10 @@
 import { useRef } from "react";
 
-import ModalPortal from "@/components/common/entities/ModalPortal";
+import ModalPortal from "@entities/ModalPortal";
 import useOutsideClick from "@/hooks/common/useOutsideClick";
-import Text from "@/components/common/shared/ui/Text";
-import Button from "../../../common/shared/ui/Button";
-import Spacing from "@/components/common/shared/layout/Spacing";
+import Text from "@shared/ui/Text";
+import Button from "@shared/ui/Button";
+import Spacing from "@shared/layout/Spacing";
 
 interface ModalProps {
   openModal: () => void;
