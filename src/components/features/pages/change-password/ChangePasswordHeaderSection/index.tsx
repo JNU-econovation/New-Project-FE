e700b9f630da31e732d453460cb7ae72991b13f6
minbo <smb5174@naver.com>
2025-06-02 09:53:58 +0900

    feat: LeftBlackArrowIcon 컴포넌트 구현

--- src/components/features/pages/change-password/ChangePasswordHeaderSection/index.tsx
+++ src/components/features/pages/change-password/ChangePasswordHeaderSection/index.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import Text from "@shared/ui/Text";
-import Left_Chevron from "@/icons/Left_Chevron.svg";
-import Image from "next/image";
 import useRouteBackBridge from "@/hooks/bridge/useRouteBackBridge";
+import LeftBlackArrowIcon from "@shared/ui/LeftBlackArrowIcon";
 
 export default function ChangePasswordHeaderSection() {
   const goBack = useRouteBackBridge();
@@ -11,7 +10,7 @@ export default function ChangePasswordHeaderSection() {
   return (
     <header className="border-b border-gray-30 flex flex-col items-center relative">
       <button onClick={goBack} className="absolute left-5">
-        <Image src={Left_Chevron} alt="화살표 아이콘" width={35} height={35} />
+        <LeftBlackArrowIcon width={35} height={35} />
       </button>
       <Text fontSize="text-2xl" className="pb-3">
         비밀번호 변경
--- src/components/features/pages/my-info/MyInfoHeaderSection/index.tsx
+++ src/components/features/pages/my-info/MyInfoHeaderSection/index.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import Text from "@shared/ui/Text";
-import Left_Chevron from "@/icons/Left_Chevron.svg";
-import Image from "next/image";
 import useRouteBackBridge from "@/hooks/bridge/useRouteBackBridge";
+import LeftBlackArrowIcon from "@shared/ui/LeftBlackArrowIcon";
 
 export default function MyInfoHeaderSection() {
   const goBack = useRouteBackBridge();
@@ -11,7 +10,7 @@ export default function MyInfoHeaderSection() {
   return (
     <header className="border-b border-gray-30 flex flex-col items-center relative pb-3">
       <button onClick={goBack} className="absolute left-5">
-        <Image src={Left_Chevron} alt="화살표 아이콘" width={35} height={35} />
+        <LeftBlackArrowIcon width={35} height={35} />
       </button>
       <Text fontSize="text-2xl">내 정보</Text>
     </header>
