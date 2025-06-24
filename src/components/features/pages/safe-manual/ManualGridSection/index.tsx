74e1f52ddd88635de72dbe637374d1c5f693e734
박건규 <geongyu09@gmail.com>
2025-06-24 13:58:45 +0900

    feat: 안전 메뉴얼 페이지에서 사용하는 section 컴포넌트 구현

---
+++ src/components/features/pages/safe-manual/ManualGridSection/index.tsx
@@ -0,0 +1,82 @@
+import Manual_Animal from "@/icons/Manual_Animal.svg";
+import Manual_Distress from "@/icons/Manual_Distress.svg";
+import Manual_Pain_Man from "@/icons/Manual_Pain_Man.svg";
+import Manual_Temperature from "@/icons/Manual_Temperature.svg";
+import Spacing from "@shared/layout/Spacing";
+import Image from "next/image";
+
+export default function ManualGridSection() {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <div className="p-4 rounded-lg shadow break-keep">
+        <span className="text-main-green font-bold text-lg text-center">
+          신체 부상
+        </span>
+        <Spacing size={8} />
+        <Image
+          src={Manual_Pain_Man}
+          alt="신체 부상"
+          width={61}
+          height={61}
+          className="self-center mx-auto"
+        />
+        <Spacing size={8} />
+        <p className="text-sm">
+          외부 충격에 의한 일반적인 부상에 대한 응급조치
+        </p>
+      </div>
+      <div className="p-4 rounded-lg shadow break-keep">
+        {/* <Text align>신체 부상</Text> */}
+        <span className="text-main-green font-bold text-lg text-center">
+          체온 및 대사 이상
+        </span>
+        <Spacing size={8} />
+        <Image
+          src={Manual_Temperature}
+          alt="체온 및 대사 이상"
+          width={61}
+          height={61}
+          className="self-center mx-auto"
+        />
+        <Spacing size={8} />
+        <span className="text-sm leading-0">
+          체온 저하 또는 탈수 등 체내 이상 징후 대응법
+        </span>
+      </div>
+      <div className="p-4 rounded-lg shadow break-keep">
+        <span className="text-main-green font-bold text-lg text-center">
+          동물 및 자연환경 피해
+        </span>
+        <Spacing size={2} />
+        <Image
+          src={Manual_Animal}
+          alt="동물 및 자연환경 피해"
+          width={61}
+          height={61}
+          className="self-center mx-auto"
+        />
+        <Spacing size={8} />
+        <span className="text-sm leading-0">
+          야생동물과 외부 환경에 의한 부상 대응
+        </span>
+      </div>
+      <div className="p-4 rounded-lg shadow break-keep">
+        <span className="text-main-green font-bold text-lg text-center">
+          조난 및 고립
+        </span>
+        <Spacing size={8} />
+        <Image
+          src={Manual_Distress}
+          alt="조난 및 고립"
+          width={61}
+          height={61}
+          className="self-center mx-auto"
+        />
+        <Spacing size={8} />
+        <span className="text-sm leading-0">
+          길을 잃거나 고립된 상황에서의 대응
+        </span>
+      </div>
+    </div>
+  );
+}
