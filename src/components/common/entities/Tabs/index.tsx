ce36591fc58d390899f74532c3aaee1289fb7188
박건규 <geongyu09@gmail.com>
2025-06-24 17:25:56 +0900

    feat: Tabs 공통 컴포넌트 구현
    Tabs 공통 컴포넌트의 경우 컴파운드 패턴을 통해서 ui와 내부 로직을 구분하게 되었습니다.
    다만, 해당 페턴을 구현하기 위해서 ui 컴포넌트역시 컨텍스트가 필요하고, 이에 따라서 provider 내부에서만 사용이 가능합니다.
    이런 제약사항으로 인하여 다른 환경에서 사용할 수 없도록 에러처리를 추가하였고, 일반적인 /components 가 아닌 Tabs 폴더 내부에 따로 정의하였습니다.
    
    #30

---
+++ src/components/common/entities/Tabs/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { PropsWithChildren, useEffect, useState } from "react";
+
+import Tab from "./components/Tab";
+import TabContent from "./components/TabContext";
+import TabList from "./components/TabLIst/index";
+import TabContext from "./context/TabContext/index";
+import type { TabContextType } from "./types";
+
+// interface TabProps extends PropsWithChildren {}
+
+export default function Tabs({ children }: PropsWithChildren) {
+  const [tabItems, setTabItems] = useState<string[]>([]);
+  const [selectedTab, setSelectedTab] = useState<string | null>(null);
+  const [selectedTabIndex, setSelectedTabIndex] = useState<number | null>(null);
+
+  const setTabItemsHandler = (items: string[]) => {
+    setTabItems(items);
+  };
+
+  const setSelectedTabHandler = (tab: string | null) => {
+    setSelectedTab(tab);
+  };
+
+  useEffect(() => {
+    setSelectedTabIndex(
+      tabItems.findIndex((item) => item === selectedTab) >= 0
+        ? tabItems.findIndex((item) => item === selectedTab)
+        : null
+    );
+  }, [selectedTab, setSelectedTabIndex, tabItems]);
+
+  const value: TabContextType = {
+    tabItems,
+    selectedTab,
+    selectedTabIndex,
+    setTabItemsHandler,
+    setSelectedTabHandler,
+  };
+
+  return <TabContext.Provider value={value}>{children}</TabContext.Provider>;
+}
+
+Tabs.Content = TabContent;
+Tabs.TabList = TabList;
+Tabs.Tab = Tab;
