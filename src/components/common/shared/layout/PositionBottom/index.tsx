b3308b80c7ce24a0ee45bc06d985ff51c923efe5
박건규 <geongyu09@gmail.com>
2025-06-26 20:44:53 +0900

    feat: PositionBottom 컴포넌트에 padding 값 추가
    #33

--- src/components/common/shared/layout/PositionBottom/index.tsx
+++ src/components/common/shared/layout/PositionBottom/index.tsx
@@ -6,16 +6,18 @@ const DEFAULT_BOTTOM_SPACE = 18; // 하단 여백
 interface PositionBottomProps extends PropsWithChildren {
   bottom?: number;
   zIndex?: number;
+  padding?: number;
 }
 
 export default function PositionBottom({
   children,
   bottom = DEFAULT_BOTTOM_SPACE,
+  padding = 0,
 }: PositionBottomProps) {
   return (
     <div
       className={cn("absolute left-0 w-full")}
-      style={{ bottom, zIndex: 10 }}
+      style={{ bottom, zIndex: 10, padding: `${padding * 0.25}rem` }}
     >
       {children}
     </div>
