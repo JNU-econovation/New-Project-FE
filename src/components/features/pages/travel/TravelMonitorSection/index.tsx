09089091df48f71fae7699d384492edef6e8e262
박건규 <geongyu09@gmail.com>
2025-06-26 20:45:26 +0900

    feat: 산행 페이지 하단에 있는 현재 산행 정보를 표현하는 컴포넌트 ui 구현
    #33

---
+++ src/components/features/pages/travel/TravelMonitorSection/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Spacing from "@shared/layout/Spacing";
+
+export default function TravelMonitorSection() {
+  return (
+    <section className="bg-white rounded-xl z-50 px-4">
+      <div className="flex justify-center items-center gap-6 -translate-y-1/2">
+        <button className="bg-main-green rounded-full w-16 h-16 flex justify-center items-center gap-2  active:scale-95 transition-transform duration-200 ease-in-out">
+          <div className="h-8 w-1.5 bg-white rounded-xs" />
+          <div className="h-8 w-1.5 bg-white rounded-xs" />
+        </button>
+        <button className="bg-main-green rounded-full w-16 h-16 flex justify-center items-center active:scale-95 transition-transform duration-200 ease-in-out">
+          <div className="bg-white w-5 h-5 rounded-xs" />
+        </button>
+      </div>
+      <p className="text-3xl font-semibold text-center">00:00:00</p>
+      <span className="text-center text-gray-20">산행 시간</span>
+
+      <Spacing size={4} />
+
+      <div className="bg-green-20 rounded-xl p-4 flex justify-between items-center ">
+        <div className="flex gap-2">
+          <span>🏃‍♂️‍➡️</span>
+          <div>
+            <p className="font-semibold text-base">1.89</p>
+            <p className="text-sm text-end">km</p>
+          </div>
+        </div>
+        <div className="h-10 border border-gray-20 opacity-50" />
+        <div className="flex gap-2">
+          <span>🔥</span>
+          <p className="font-semibold text-base">03:24</p>
+        </div>
+        <div className="h-10 border border-gray-20 opacity-50" />
+        <div className="flex gap-2">
+          <p>⛰️</p>
+          <p className="font-semibold text-base">01:20</p>
+        </div>
+      </div>
+
+      <Spacing size={4} />
+    </section>
+  );
+}
