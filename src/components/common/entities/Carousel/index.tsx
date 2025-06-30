44d290b8c15ea8233f60fd034a776749fb4247dc
박건규 <geongyu09@gmail.com>
2025-06-30 10:18:59 +0900

    chore: 캐러셀 네비게이션바와 간격 조정

--- src/components/common/entities/Carousel/index.tsx
+++ src/components/common/entities/Carousel/index.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { cn } from "@/utils/cn";
+import Spacing from "@shared/layout/Spacing";
 import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface CarouselProps {
   items: ReactNode[];
 }
 
+//TODO: 캐러샐 ui를 사용하는 곳에서 변경 가능하게 수정 필요
 export default function Carousel({ items }: CarouselProps) {
   const [prevX, setPrevX] = useState(0);
   const [x, setX] = useState(0);
@@ -85,7 +87,8 @@ export default function Carousel({ items }: CarouselProps) {
           </div>
         ))}
       </div>
-      <div className="flex items-center justify-center gap-2 mt-4">
+      <Spacing size={1} />
+      <div className="flex items-center justify-center gap-2">
         {items.map((_, index) => (
           <div
             className={cn("w-2 h-2 rounded-full", {
