7754bad243b7fc397962ce976500fbcf6173f92d
박건규 <geongyu09@gmail.com>
2025-07-02 14:14:44 +0900

    chore: Text 공통 컴포넌트의 상위에서 받는 스타일 타입값을 컴포넌트 내부에서 /types로 변경함에 따라 import 해오도록 수정
    #37

--- src/components/common/shared/ui/Text/index.tsx
+++ src/components/common/shared/ui/Text/index.tsx
@@ -1,3 +1,11 @@
+import {
+  ColorType,
+  OpacityType,
+  SizeType,
+  TextAlignType,
+  WeightType,
+  ZIndexType,
+} from "@/types/css";
 import { cn } from "@/utils/cn";
 
 interface TextProps {
@@ -9,66 +17,8 @@ interface TextProps {
   display?: "block" | "inline";
   align?: TextAlignType;
   opacity?: OpacityType;
-  zIndex?:
-    | "z-0"
-    | "z-10"
-    | "z-20"
-    | "z-30"
-    | "z-40"
-    | "z-50"
-    | "-z-10"
-    | "-z-20"
-    | "-z-30"
-    | "-z-40"
-    | "-z-50";
+  zIndex?: ZIndexType;
 }
-//TODO: text 뺴기
-
-type ColorType =
-  | "text-main-green"
-  | "text-sub-gray"
-  | "text-white"
-  | "text-black"
-  | "text-error-message";
-
-//TODO: 숫자로 크기 다룰 수 있게 하기
-type SizeType =
-  | "text-xs"
-  | "text-sm"
-  | "text-base"
-  | "text-xl"
-  | "text-xl"
-  | "text-2xl"
-  | "text-3xl"
-  | "text-4xl"
-  | "text-6xl"
-  | "text-7xl";
-
-type WeightType =
-  | "font-thin"
-  | "font-extralight"
-  | "font-light"
-  | "font-normal"
-  | "font-medium"
-  | "font-semibold"
-  | "font-bold"
-  | "font-extrabold"
-  | "font-black";
-
-type TextAlignType = "text-left" | "text-center" | "text-right";
-
-type OpacityType =
-  | "opacity-0"
-  | "opacity-10"
-  | "opacity-20"
-  | "opacity-30"
-  | "opacity-40"
-  | "opacity-50"
-  | "opacity-60"
-  | "opacity-70"
-  | "opacity-80"
-  | "opacity-90"
-  | "opacity-100";
 
 export default function Text({
   children,
