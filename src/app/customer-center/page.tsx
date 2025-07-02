7e2ac569385ab89074c3323b54cebe632961e9bf
박건규 <geongyu09@gmail.com>
2025-07-02 15:45:41 +0900

    feat: 고객 센터 페이지 구현
    #37

---
+++ src/app/customer-center/page.tsx
@@ -0,0 +1,9 @@
+import CustomerCenterTab from "@widgets/CustomerCenterTab";
+
+export default function CustomerCenterPage() {
+  return (
+    <div className="px-6">
+      <CustomerCenterTab />
+    </div>
+  );
+}
