515d578152dcadfd1db4b5ed855b34acdb21806b
박건규 <geongyu09@gmail.com>
2025-07-03 18:16:52 +0900

    feat: 캘린더 접근성 aria 속성 추가
    #37

--- src/components/features/pages/my-info/TravelLogCalendarSection/index.tsx
+++ src/components/features/pages/my-info/TravelLogCalendarSection/index.tsx
@@ -7,8 +7,4 @@ import WeekPolygonIcon from "@icons/WeekPolygonIcon";
 export default function TravelLogCalendarSection() {
   //TODO: content는 서버에서 받은 값에서 등산기록이 있는 날인지 확인 후 switchcase로 하면 됨
   return <Calendar content={() => <WeekPolygonIcon />} />;
-  {
-    /* {day !== null && <PolygonIcon />} */
-  }
-  // {day !== null && <WeekPolygonIcon />}
 }
