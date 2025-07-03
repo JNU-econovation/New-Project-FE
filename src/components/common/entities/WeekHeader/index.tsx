0dbe9941bb761915a8bc18a1b8f2090578439310
박건규 <geongyu09@gmail.com>
2025-07-03 16:04:38 +0900

    feat: 헤더 공통 컴포넌트 구현
    #37

---
+++ src/components/common/entities/WeekHeader/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Text from "@shared/ui/Text";
+import useRouteBackBridge from "@/hooks/bridge/useRouteBackBridge";
+import LeftChevronIcon from "@/components/common/shared/ui/icons/LeftChevronIcon";
+
+interface WeekHeaderProps {
+  headerText?: string;
+}
+
+export default function WeekHeader({ headerText }: WeekHeaderProps) {
+  const goBack = useRouteBackBridge();
+
+  return (
+    <header className="border-gray-30 flex flex-col items-center relative pb-3">
+      <button onClick={goBack} className="absolute left-0">
+        <LeftChevronIcon />
+      </button>
+      <Text fontSize="text-base">{headerText}</Text>
+    </header>
+  );
+}
