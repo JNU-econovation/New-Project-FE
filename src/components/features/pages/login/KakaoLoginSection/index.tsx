e11387ca23a014c3b8395ee671f93fa67465af77
박건규 <geongyu09@gmail.com>
2025-07-05 22:19:18 +0900

    fix: 보안 문제를 막기 위하여 반환된 uri로 이동시 유효한 url인지 확인 후 router.replace로 이동하도록 수정
    #51

--- src/components/features/pages/login/KakaoLoginSection/index.tsx
+++ src/components/features/pages/login/KakaoLoginSection/index.tsx
@@ -3,8 +3,10 @@
 import useLogBridge from "@hooks/bridge/useLogBridge";
 import useRouteBackBridge from "@hooks/bridge/useRouteBackBridge";
 import useKakaoLoginQuery from "@hooks/query/useKakaoLoginQuery";
+import { useRouter } from "next/navigation";
 
 export default function KakaoLoginSection() {
+  const router = useRouter();
   const { data: kakaoLoginUrl, error, isLoading } = useKakaoLoginQuery();
   const log = useLogBridge();
   const routeBack = useRouteBackBridge();
@@ -20,7 +22,18 @@ export default function KakaoLoginSection() {
     return null;
   }
 
-  window.location.href = kakaoLoginUrl.uri;
+  if (
+    !kakaoLoginUrl.uri.startsWith("https://kauth.kakao.com/oauth/authorize")
+  ) {
+    console.error("유효하지 않은 카카오 로그인 URL입니다.");
+    console.error("카카오 로그인 URL:", kakaoLoginUrl.uri);
+    log("유효하지 않은 카카오 로그인 URL입니다.");
+    log(kakaoLoginUrl.uri);
+    routeBack();
+    return null;
+  }
+
+  router.replace(kakaoLoginUrl.uri);
 
   return null;
 }
