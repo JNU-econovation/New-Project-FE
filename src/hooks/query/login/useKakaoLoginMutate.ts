20d71385922734ba151413b1a81f0f302b6f7325
박건규 <geongyu09@gmail.com>
2025-07-05 17:25:39 +0900

    chore: 더 이상 사용하지 않는 useKakaoLogin, useKakaoLoginMutate 훅 제거
    #51

--- src/hooks/query/login/useKakaoLoginMutate.ts
+++
@@ -1,9 +0,0 @@
-import postKakaoLogin, { KAKAO_LOGIN_URI } from "@api/v1/oauth/kakao";
-import { useMutation } from "@tanstack/react-query";
-
-export const useKakaoLoginMutate = () => {
-  return useMutation({
-    mutationKey: [KAKAO_LOGIN_URI],
-    mutationFn: postKakaoLogin,
-  });
-};
