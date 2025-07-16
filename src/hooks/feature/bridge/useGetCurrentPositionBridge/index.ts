7ab5077daec548f0ad24b6a076c75cc3c5ee4108
박건규 <geongyu09@gmail.com>
2025-07-16 22:28:16 +0900

    chore: useBridge 훅 위치 변경에 따른 inport 문 수정
    #57

--- src/hooks/feature/bridge/useGetCurrentPositionBridge/index.ts
+++ src/hooks/feature/bridge/useGetCurrentPositionBridge/index.ts
@@ -1,9 +1,9 @@
-import { useBridge } from "@/service/bridge/hooks/useBridge";
 import { useCallback } from "react";
 import type {
   MessageEventRequestData,
   MessageEventResponseData,
 } from "@/types/bridge";
+import { useBridge } from "@/service/bridge";
 
 interface Coords {
   accuracy: number;
