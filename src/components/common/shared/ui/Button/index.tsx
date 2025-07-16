9c55c2bfa1a41259f4fc45962ed64ae881cbec5d
박건규 <geongyu09@gmail.com>
2025-07-16 11:09:44 +0900

    design: 버튼 컴포넌트 레이아웃 변경
    #37

--- src/components/common/shared/ui/Button/index.tsx
+++ src/components/common/shared/ui/Button/index.tsx
@@ -3,31 +3,34 @@ import { cva, VariantProps } from "class-variance-authority";
 
 import { cn } from "@/utils/cn";
 
-export const ButtonVariants = cva(`rounded-xl`, {
-  variants: {
-    color: {
-      green: "bg-main-green text-white",
-      kakaoYellow: "bg-kakao-yellow text-black",
-      black: "bg-black text-white",
-      white: "bg-white text-black",
-      gray: "bg-gray-200 text-black",
+export const ButtonVariants = cva(
+  `rounded-xl shrink-0 active:opacity-95 active:scale-[99%] transition-all duration-200`,
+  {
+    variants: {
+      color: {
+        green: "bg-main-green text-white",
+        kakaoYellow: "bg-kakao-yellow text-black",
+        black: "bg-black text-white",
+        white: "bg-white text-black",
+        gray: "bg-gray-30 text-white",
+      },
+      size: {
+        lg: "px-4 py-2 font-bold text-lg",
+        md: "px-5 py-2.5 text-base font-semibold",
+        sm: "px-4 py-2 text-sm font-semibold",
+      },
+      fullWidth: {
+        true: "w-full p-3 text-center grow",
+        false: "",
+      },
     },
-    size: {
-      lg: "px-4 py-2 font-bold text-lg",
-      md: "px-4 py-2 text-base font-semibold",
-      sm: "px-4 py-2 text-sm",
+    defaultVariants: {
+      color: "green",
+      size: "lg",
+      fullWidth: false,
     },
-    fullWidth: {
-      true: "w-full p-3",
-      false: "",
-    },
-  },
-  defaultVariants: {
-    color: "green",
-    size: "lg",
-    fullWidth: false,
-  },
-});
+  }
+);
 
 interface ButtonProps
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "color" | "size">,
