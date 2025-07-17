f68bbb1b3b252f7d0aa8d22fafa1774fd84330b2
박건규 <geongyu09@gmail.com>
2025-07-17 22:48:45 +0900

    feat: CourseTabSection 컴포넌트를 Suspense.with를 사용하여 query를 사용하도록 재 구현
    #57

--- src/components/features/pages/course/CourseTabBarSection/index.tsx
+++ src/components/features/pages/course/CourseTabBarSection/index.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import Spacing from "@/components/common/shared/layout/Spacing";
-import CourseMetaDataUi from "@/components/common/shared/ui/CourseMetaDataUi";
 import ROUTE from "@/constants/route";
-import useRouteBridge from "@/hooks/feature/bridge/useRouteBridge";
 import { cn } from "@/utils/cn";
+import useRouteBridge from "@hooks/feature/bridge/useRouteBridge";
+import useCoursesOfMountainQuery from "@hooks/feature/query/useCoursesOfMountainQuery";
+import Spacing from "@shared/layout/Spacing";
+import CourseList from "@shared/ui/CourseList";
+import { Suspense } from "@suspensive/react";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
+
+import CourseTabBarSectionSkeleton from "./CourseTabBarSection.skeleton";
 
 const tabTitleList = [
   { title: "내 맞춤형", sort: "my" },
@@ -14,66 +18,65 @@ const tabTitleList = [
   { title: "난이도순", sort: "difficulty" },
 ] as const;
 
-export default function CourseTabSection() {
-  const router = useRouter();
-  const { mountainName } = useParams<{ mountainName: string }>();
-  const searchParams = useSearchParams();
-  const sortBy = searchParams.get("sort");
-  const routeCourseDetail = useRouteBridge({
-    path: "course-detail",
-    routeType: "push",
-  });
+export default Suspense.with(
+  {
+    name: "CourseTabSection",
+    clientOnly: true,
+    fallback: <CourseTabBarSectionSkeleton />,
+  },
+  function CourseTabSection() {
+    const router = useRouter();
+    const { mountainId } = useParams<{ mountainId: string }>();
+    const searchParams = useSearchParams();
+    const sortBy = searchParams.get("sort");
+    const routeCourseDetail = useRouteBridge({
+      path: "course-detail",
+      routeType: "push",
+    });
 
-  // TODO: 서버에서 데이터 받아오기
-  const courseList = new Array(10).fill(0);
+    const { data: courseList } = useCoursesOfMountainQuery(mountainId);
 
-  return (
-    <section className="flex flex-col flex-1 overflow-hidden">
-      <div className="flex justify-between items-center px-6">
-        {tabTitleList.map(({ title: tabTitle, sort }, index) => (
-          <button
-            key={`${index}-${tabTitle}`}
-            onClick={() => {
-              router.replace(
-                `${ROUTE.COURSE_SEARCH_RESULT({
-                  mountainName,
-                  sort,
-                })}`
-              );
-            }}
-          >
-            <div
-              className={cn("px-3 py-1 text-white rounded-full text-sm", {
-                "bg-gray-400": sort !== sortBy,
-                "bg-main-green": sort === sortBy,
-              })}
+    const { courses } = courseList;
+
+    return (
+      <section className="flex flex-col flex-1 overflow-hidden">
+        <div className="flex justify-between items-center px-6">
+          {tabTitleList.map(({ title: tabTitle, sort }, index) => (
+            <button
+              key={`${index}-${tabTitle}`}
+              onClick={() => {
+                router.replace(
+                  `${ROUTE.COURSE_SEARCH_RESULT({
+                    mountainId,
+                    sort,
+                  })}`
+                );
+              }}
             >
-              {tabTitle}
-            </div>
-          </button>
-        ))}
-      </div>
-      <Spacing size={4} />
-      <ul className="flex flex-col gap-4 bg-gray-200 p-6 overflow-y-auto flex-1">
-        {courseList.map((_, index) => (
-          <li
-            className="flex gap-4 justify-between bg-white rounded-lg p-3"
-            key={index} //TODO: key 수정 필요
-          >
+              <div
+                className={cn("px-3 py-1 text-white rounded-full text-sm", {
+                  "bg-gray-400": sort !== sortBy,
+                  "bg-main-green": sort === sortBy,
+                })}
+              >
+                {tabTitle}
+              </div>
+            </button>
+          ))}
+        </div>
+        <Spacing size={4} />
+        <ul className="flex flex-col gap-4 bg-gray-200 p-6 overflow-y-auto flex-1">
+          {courses.map(({ id, ...props }, index) => (
             <div
-              className="flex flex-col justify-between grow"
+              key={`${id}-${index}`}
               onClick={routeCourseDetail}
               typeof="button"
             >
-              <span className="text-lg font-semibold">
-                증심사 - 중머리재 - 원효분소
-              </span>
-              <CourseMetaDataUi time={4} distance={41} difficulty="normal" />
+              <CourseList {...props} />
             </div>
-            <div className="w-24 h-24 bg-slate-100 rounded-xl" />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-}
+          ))}
+        </ul>
+      </section>
+    );
+  }
+);
