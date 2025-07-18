7b2c1ec17d5ae64944f65c42ebd737979ec165d0
박건규 <geongyu09@gmail.com>
2025-07-18 20:37:35 +0900

    feat: 산에 대한 코스를 조회하는 쿼리 훅의 staleTime, gcTime 추가
    산에 대해 값이 변하지 않을 것이므로, gcTime을 1시간으로 길게 설정하였습니다.
    
    #57

--- src/hooks/feature/query/useCoursesOfMountainQuery/index.tsx
+++ src/hooks/feature/query/useCoursesOfMountainQuery/index.tsx
@@ -1,13 +1,15 @@
 import {
   COURSES_OF_MOUNTAIN,
   getCoursesOfMountainApi,
-} from "@/api/v1/mountains/[mountainId]/courses";
-import { useQuery } from "@tanstack/react-query";
+} from "@api/v1/mountains/[mountainId]/courses";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 const useCoursesOfMountainQuery = (mountainId: string) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: [COURSES_OF_MOUNTAIN(mountainId)],
     queryFn: () => getCoursesOfMountainApi(mountainId),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    gcTime: 1000 * 60 * 60, // 1 hour
   });
 };
 
