5f2a0628c335e1dc140aa53b8b80e3afc47af282
박건규 <geongyu09@gmail.com>
2025-07-18 20:50:49 +0900

    feat: 북마크 기능이 들어간 코스 리스트 UI 컴포넌트 구현
    #57

---
+++ src/components/features/widgets/CourseListWithBookmarkMutate/index.tsx
@@ -0,0 +1,34 @@
+import type { CourseDifficulty } from "@/types/course";
+import useBookmarkMutation from "@hooks/feature/query/useBookmarkMutation";
+import CourseList from "@shared/ui/CourseList";
+
+interface CourseListWithBookmarkMutateProps {
+  id: string;
+  name: string;
+  duration: number;
+  length: number;
+  difficulty: CourseDifficulty;
+  bookmark: boolean;
+  onSetStared?: () => void;
+  onResetStared?: () => void;
+}
+
+export default function CourseListWithBookmarkMutate({
+  id,
+  bookmark,
+  ...props
+}: CourseListWithBookmarkMutateProps) {
+  const { mutate: postBookmark } = useBookmarkMutation();
+  const deleteBookmark = (courseId: string) => {
+    console.log(courseId);
+  };
+
+  return (
+    <CourseList
+      {...props}
+      stared={bookmark}
+      onSetStared={() => postBookmark(id)}
+      onResetStared={() => deleteBookmark(id)}
+    />
+  );
+}
