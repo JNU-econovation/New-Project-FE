6e3c83854ce4b5ffe4ab1ed4bbfd0bb6f8ff5f08
박건규 <geongyu09@gmail.com>
2025-07-20 21:19:51 +0900

    feat: LeftChevronIcon 에 default width, height 값 추가
    #63

--- src/components/common/shared/ui/icons/LeftChevronIcon/index.tsx
+++ src/components/common/shared/ui/icons/LeftChevronIcon/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { ComponentProps } from "react";
 
 const DEFAULT_ALT = "왼쪽 화살표";
+const DEFAULT_WIDTH = 32;
+const DEFAULT_HEIGHT = 32;
 
 interface LeftChevronIconProps
   extends Omit<
@@ -10,22 +12,19 @@ interface LeftChevronIconProps
     "src" | "width" | "height" | "alt"
   > {
   alt?: string;
-  width?: number;
-  height?: number;
 }
 
 export default function LeftChevronIcon({
   alt = DEFAULT_ALT,
-  width,
-  height,
   ...props
 }: LeftChevronIconProps) {
   return (
     <Image
       src={Left_Chevron}
       alt={alt}
-      width={width}
-      height={height}
+      width={DEFAULT_WIDTH}
+      height={DEFAULT_HEIGHT}
+      className="min-w-[32px] min-h-[32px]"
       {...props}
     />
   );
