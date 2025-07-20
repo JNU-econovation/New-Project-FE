bb72d586c400f6eb0aa09d526818e0bbe81f8dac
박건규 <geongyu09@gmail.com>
2025-07-21 01:06:15 +0900

    feat: CourseDetailMapSection 컴포넌트에서 받아온 시설 데이터를 보여줄 수 있도록 기능 구현

--- src/components/features/pages/map/CourseDetailMapSection/index.tsx
+++ src/components/features/pages/map/CourseDetailMapSection/index.tsx
@@ -1,16 +1,47 @@
 "use client";
 
+import MAP from "@/constants/map";
+import useFacilitiesQuery from "@/hooks/feature/query/useFacilitiesQuery";
+import { getFacilitiesByFacilityType } from "@/utils/map";
+import { Suspense } from "@suspensive/react";
 import dynamic from "next/dynamic";
+import { useParams, useSearchParams } from "next/navigation";
 
 const MapWithCurrentPositionMark = dynamic(
   () => import("@widgets/MapWithCurrentPositionMark"),
   { ssr: false }
 );
 
-export default function CourseDetailMapSection() {
-  return (
-    <div className="absolute top-0 left-0 w-full h-full">
-      <MapWithCurrentPositionMark />
-    </div>
-  );
-}
+export default Suspense.with(
+  {
+    fallback: <div className="w-full h-full bg-gray-30" />,
+    name: "CourseDetailMapSection",
+  },
+  function CourseDetailMapSection() {
+    const params = useParams<{
+      mountainId: string;
+      courseId: string;
+    }>();
+    const searchParams = useSearchParams();
+
+    const { mountainId } = params;
+    const selectedTagId =
+      (searchParams.get("tag") as keyof typeof MAP.BASE_AND_FACILITY) ||
+      MAP.BASE.facilityName;
+
+    const { data } = useFacilitiesQuery({ mountainId });
+
+    const { facilities } = data;
+
+    return (
+      <div className="absolute top-0 left-0 w-full h-full">
+        <MapWithCurrentPositionMark
+          // path={data?.path}
+          markers={getFacilitiesByFacilityType(facilities, selectedTagId)}
+          currentPositionIcon={true}
+          zoom={13}
+        />
+      </div>
+    );
+  }
+);
