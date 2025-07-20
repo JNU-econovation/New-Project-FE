c7a296d75990f4b48c1028726b2203fac6bdcb64
박건규 <geongyu09@gmail.com>
2025-07-20 21:22:05 +0900

    feat: 변경된 ui 에 맞추어 CourseDetailMapTagHeaderSection ui 변경

--- src/components/features/pages/map/CourseDetailMapTagHeaderSection/index.tsx
+++ src/components/features/pages/map/CourseDetailMapTagHeaderSection/index.tsx
@@ -2,6 +2,7 @@
 
 import ROUTE from "@/constants/route";
 import MapHeaderTag from "@shared/ui/MapHeaderTag";
+import BackButton from "@widgets/BackButton";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { useMemo } from "react";
 
@@ -28,19 +29,26 @@ export default function CourseDetailMapTagHeaderSection() {
   );
 
   return (
-    <section className="flex max-w-full overflow-x-scroll gap-4 hide-scrollbar">
-      {mapHeaderTags.map(({ id, text }) => (
-        <MapHeaderTag
-          key={text}
-          text={text}
-          isSelected={id === selectedTagId}
-          onClickHandler={() => {
-            router.replace(
-              ROUTE.MOUNTAIN_COURSE_DETAIL(mountainId, courseId) + "?tag=${id}"
-            );
-          }}
-        />
-      ))}
+    <section className="flex w-full overflow-hidden gap-4">
+      <BackButton />
+      <div className="flex-1 min-w-0">
+        <ul className="flex hide-scrollbar overflow-x-scroll gap-4">
+          {mapHeaderTags.map(({ id, text }) => (
+            <li key={text} className="flex-shrink-0">
+              <MapHeaderTag
+                text={text}
+                isSelected={id === selectedTagId}
+                onClickHandler={() => {
+                  router.replace(
+                    ROUTE.MOUNTAIN_COURSE_DETAIL(mountainId, courseId) +
+                      `?tag=${id}`
+                  );
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
