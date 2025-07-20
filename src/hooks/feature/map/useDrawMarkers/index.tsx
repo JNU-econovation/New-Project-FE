b1677ccda963f5b0fff18b2a54b39c7ffb935c2e
박건규 <geongyu09@gmail.com>
2025-07-21 02:25:36 +0900

    feat: useDrawMarkers가 시설뿐만 아닌 다른 것까지 그릴 수 있도록 타입 변경
    #63

--- src/hooks/feature/map/useDrawMarkers/index.tsx
+++ src/hooks/feature/map/useDrawMarkers/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { FacilityMarker } from "@/types/map";
+// import type { FacilityMarker } from "@/types/map";
+import type { Markers } from "@/types/map";
 import { useEffect } from "react";
 
 interface UseDrawMarkersProps {
   map: any;
-  markers: FacilityMarker[];
+  markers: Markers[];
   enable?: boolean;
 }
 const useDrawMarkers = ({ map, markers, enable }: UseDrawMarkersProps) => {
@@ -13,16 +14,15 @@ const useDrawMarkers = ({ map, markers, enable }: UseDrawMarkersProps) => {
 
     const drawnMarkers: naver.maps.Marker[] = [];
 
-    markers.forEach(
-      ({ coordinate, facilityId, facilityName, facilityType }) => {
-        const color = "#0000FF";
+    markers.forEach(({ coordinate, name }) => {
+      const color = "#0000FF";
 
-        const marker = new naver.maps.Marker({
-          position: new naver.maps.LatLng(coordinate[0], coordinate[1]),
-          map: map,
-          title: facilityName,
-          icon: {
-            content: `<div style="
+      const marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(coordinate[1], coordinate[0]),
+        map: map,
+        title: name,
+        icon: {
+          content: `<div style="
             background: white; 
             border: 2px solid ${color}; 
             border-radius: 20px; 
@@ -32,16 +32,13 @@ const useDrawMarkers = ({ map, markers, enable }: UseDrawMarkersProps) => {
             color: ${color};
             box-shadow: 0 2px 4px rgba(0,0,0,0.2);
             white-space: nowrap;
-          ">${facilityName}</div>`,
-            size: new naver.maps.Size(100, 30),
-            anchor: new naver.maps.Point(50, 15),
-          },
-        });
-        drawnMarkers.push(marker);
-      }
-    );
-
-    console.log("Drawn Markers: ", drawnMarkers);
+          ">${name}</div>`,
+          size: new naver.maps.Size(100, 30),
+          anchor: new naver.maps.Point(50, 15),
+        },
+      });
+      drawnMarkers.push(marker);
+    });
 
     return () => {
       drawnMarkers.forEach((marker) => {
