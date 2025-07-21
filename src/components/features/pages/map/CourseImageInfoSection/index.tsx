5c995a5bf7b56720ea5a3b0defb36e0f900ebbf5
박건규 <geongyu09@gmail.com>
2025-07-21 17:07:14 +0900

    feat: 거점의 사진 secction에서 사진 데이털르 서버에서 가져오도록 기능 추가
    #63

--- src/components/features/pages/map/CourseImageInfoSection/index.tsx
+++ src/components/features/pages/map/CourseImageInfoSection/index.tsx
@@ -1,16 +1,61 @@
-import Mock_Mountain from "@/images/Mock_Mountain.png";
-import Mock_Mountain2 from "@/images/Mock_Mountain2.png";
+"use client";
+
+import useBasesDetailQuery from "@/hooks/feature/query/useBasesDetailQuery";
+import { Suspense } from "@suspensive/react";
+import {
+  useParams,
+  useSearchParams,
+} from "next/dist/client/components/navigation";
 import Image from "next/image";
 
-export default function CourseImageInfoSection() {
-  return (
-    <div className="grid grid-cols-2 bg-green-10 rounded-2xl p-2">
-      <div className="flex items-center justify-center w-full h-full">
-        <Image src={Mock_Mountain} alt="코스 상세 사진" />
+export default Suspense.with(
+  {
+    fallback: (
+      <div className="h-10 w-full animate-pulse bg-gray-100 rounded-2xl border border-main-green opacity-50" />
+    ),
+    name: "CourseImageInfoSection",
+  },
+  () => {
+    const { mountainId } = useParams<{
+      mountainId: string;
+      courseId: string;
+    }>();
+    const searchParams = useSearchParams();
+    const { data } = useBasesDetailQuery({ mountainId });
+
+    const selectedBaseId =
+      searchParams.get("baseId") || data.baseDetails[0].baseId;
+    const { baseDetails } = data;
+
+    const baseDetail = baseDetails.find(
+      ({ baseId }) => `${baseId}` === `${selectedBaseId}`
+    );
+
+    if (!baseDetail) {
+      return <div className="p-4">기본 정보가 없습니다.</div>;
+    }
+
+    const { images } = baseDetail;
+    return (
+      //height값이 고정되어있습니다! 주의해주세요! (h-28)
+      <div className="grid grid-cols-2 bg-green-10 rounded-2xl p-2 h-28">
+        <div className="flex items-center justify-center w-full h-full">
+          <Image
+            src={images[0]}
+            alt="코스 상세 사진"
+            width={110}
+            height={100}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image
+            src={images[1]}
+            alt="코스 상세 사진"
+            width={110}
+            height={110}
+          />
+        </div>
       </div>
-      <div className="flex items-center justify-center">
-        <Image src={Mock_Mountain2} alt="코스 상세 사진" />
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
--- src/components/features/pages/map/CourseWeatherClothesInfoSection/index.tsx
+++ src/components/features/pages/map/CourseWeatherClothesInfoSection/index.tsx
@@ -11,7 +11,9 @@ import { useParams, useSearchParams } from "next/navigation";
 export default Suspense.with(
   {
     // TODO: 스캘레톤 컴포넌트로 변경
-    fallback: <div className="h-8 w-full bg-gray-200 animate-pulse" />,
+    fallback: (
+      <div className="w-full bg-green-10 animate-pulse h-28 opacity-20 rounded-2xl" />
+    ),
     name: "CourseWeatherClothesInfoSection",
   },
   () => {
@@ -37,24 +39,25 @@ export default Suspense.with(
     const { weather, recommendedOutfit } = baseDetail;
 
     return (
-      <div className="grid grid-cols-2 bg-green-10 rounded-2xl p-2.5">
+      // height값이 고정되어있습니다! 주의해주세요!
+      <div className="grid grid-cols-2 bg-green-10 rounded-2xl p-2.5 h-28">
         <p className="font-bold text-gray-20">기상 정보</p>
         <p className="font-bold text-gray-20">산행 복장</p>
         <div className="flex items-center justify-around">
           <SunnyIcon alt="맑은 날씨" />
           <div>
-            <p className="text-4xl font-extralight">
+            <p className="text-3xl font-extralight">
               {"18"}
-              <span className="text-3xl">&deg;C</span>
+              <span className="text-2xl">&deg;C</span>
             </p>
-            <p className="text-xl">{weather}</p>
+            <p className="text-sm font-bold">{weather}</p>
           </div>
         </div>
         <div className="flex items-center gap-2">
           <ClothesIcon alt="옷 아이콘" />
           <div>
             <p className="text-xl font-bold">
-              {recommendedOutfit ?? "추천 복장 없음"}
+              {recommendedOutfit ?? "추천 복장"}
             </p>
             <p className="text-sm">긴팔 긴바지 착용 권장</p>
           </div>
