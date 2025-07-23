2458ae4939bdff36922051d9b4f52ad453ede870
박건규 <geongyu09@gmail.com>
2025-07-23 22:48:56 +0900

    faet: publicApi에서 nginx로 요청할 때 올바른 응답을 기대하기 위하여 헤더 추가
    #68

--- src/api/_instances/publicApi/index.ts
+++ src/api/_instances/publicApi/index.ts
@@ -3,16 +3,22 @@ import axios, { AxiosResponse } from "axios";
 const publicApi = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`?.replace(/\/$/, ""),
   timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "*/*",
-  },
+  headers:
+    process.env.NODE_ENV === "development"
+      ? {
+          "ngrok-skip-browser-warning": "true",
+          "Content-Type": "application/json",
+          Accept: "*/*",
+        }
+      : {
+          "Content-Type": "application/json",
+          Accept: "*/*",
+        },
 });
 
 publicApi.interceptors.response.use(
   (response: AxiosResponse) => {
-    response.data = response.data.data || {};
-    return response;
+    return response.data;
   },
   async (error) => {
     return Promise.reject(error);
