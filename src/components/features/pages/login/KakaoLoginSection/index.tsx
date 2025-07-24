1c8f4de2164fccafecd8e56859aa854984c0baa2
박건규 <geongyu09@gmail.com>
2025-07-24 15:04:17 +0900

    chore: 불필요한 코드 제거

--- src/components/features/pages/login/KakaoLoginSection/index.tsx
+++ src/components/features/pages/login/KakaoLoginSection/index.tsx
@@ -17,7 +17,7 @@ export default function KakaoLoginSection() {
       text1: "카카오 로그인 실패",
       text2: "로그인에 실패했습니다. 잠시 후 다시 시도해주세요.",
     });
-    alert("카카오 로그인에 실패했습니다. 잠시 후 다시 시도해주세요.");
+    // alert("카카오 로그인에 실패했습니다. 잠시 후 다시 시도해주세요.");
     goBack();
     return (
       <div className="w-screen h-screen flex items-center justify-center">
@@ -30,7 +30,7 @@ export default function KakaoLoginSection() {
 
   if (!isLoading && data && data.uri) {
     const { uri } = data;
-    alert(uri);
+    // alert(uri);
     window.location.href = uri;
     return null;
   }
