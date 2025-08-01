b5c11e6847f7d48706c6f3cf3915ba03114c1cd7
박건규 <geongyu09@gmail.com>
2025-08-02 01:19:00 +0900

    feat: MountainBgItem 를 사용하여 MountainGridSection 구현
    #56

--- src/components/features/pages/course/MountainGridSection/index.tsx
+++ src/components/features/pages/course/MountainGridSection/index.tsx
@@ -3,7 +3,7 @@
 import ROUTE from "@/constants/route";
 import { StackLink } from "@/service/StackLink";
 import Spacing from "@shared/layout/Spacing";
-import Text from "@shared/ui/Text";
+import MountainBgItem from "@shared/ui/MountainBgItem";
 
 export default function MountainGridSection() {
   return (
@@ -11,42 +11,14 @@ export default function MountainGridSection() {
       <div className="flex flex-col w-full h-fit gap-4">
         {/* TODO: href 하드코딩 피하기 */}
         <StackLink href={ROUTE.MOUNTAIN_COURSE("1") + "?sort=my"} preLoad>
-          <div
-            className="h-72 rounded-2xl flex items-center justify-center relative overflow-hidden"
-            style={{
-              backgroundImage: `url('/images/mudeungsan.jpg')`, //TODO: 하드 코딩 없애기 -> 정적 이미지 주소를 관리하는 방법 고안 필요
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            <div className="absolute top-0 right-0 bg-black opacity-40 w-full h-full" />
-            <div className="absolute top-6 right-4">
-              <Text
-                fontSize="text-4xl"
-                fontWeight="font-bold"
-                color="text-white"
-                zIndex="z-10"
-              >
-                광주
-              </Text>
-            </div>
-            <Text
-              fontSize="text-4xl"
-              fontWeight="font-bold"
-              color="text-white"
-              zIndex="z-10"
-            >
-              무등산
-            </Text>
-          </div>
+          <MountainBgItem region="광주" mountainName="무등산" />
         </StackLink>
-        {/* TODO: 실제 산으로 변경하기 */}
-        <div className="h-72 bg-amber-100 rounded-2xl" />
+        <MountainBgItem locked />
       </div>
       <div className="flex flex-col w-full h-fit gap-4">
         <Spacing size={12} />
-        <div className="h-72 bg-amber-100 rounded-2xl" />
-        <div className="h-72 bg-amber-100 rounded-2xl" />
+        <MountainBgItem locked />
+        <MountainBgItem locked />
       </div>
     </section>
   );
