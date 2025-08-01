db177fda6e3a10502af9638472d67b1d8dd119b4
박건규 <geongyu09@gmail.com>
2025-08-02 00:05:25 +0900

    feat: CourseDetailPrefetcher 적용
    #56

--- src/app/mountain/[mountainId]/course/page.tsx
+++ src/app/mountain/[mountainId]/course/page.tsx
@@ -1,3 +1,4 @@
+import CourseDetailPrefetcher from "@pages/course/CourseDetailPrefetcher";
 import CourseSearchBarSection from "@pages/course/CourseSearchNarSection";
 import CourseTabSection from "@pages/course/CourseTabBarSection";
 import Flex from "@shared/layout/Flex";
@@ -6,16 +7,19 @@ import BackButton from "@widgets/route/BackButton";
 
 export default function MountainCoursePage() {
   return (
-    <Flex flexDirection="flex-col" height={"screen"}>
-      <Flex flexDirection="flex-col">
-        <div className="px-6 pt-12">
-          <BackButton />
-          <Spacing size={8} />
-          <CourseSearchBarSection />
-          <Spacing size={4} />
-        </div>
+    <>
+      <Flex flexDirection="flex-col" height={"screen"}>
+        <Flex flexDirection="flex-col">
+          <div className="px-6 pt-12">
+            <BackButton />
+            <Spacing size={8} />
+            <CourseSearchBarSection />
+            <Spacing size={4} />
+          </div>
+        </Flex>
+        <CourseTabSection />
       </Flex>
-      <CourseTabSection />
-    </Flex>
+      <CourseDetailPrefetcher />
+    </>
   );
 }
