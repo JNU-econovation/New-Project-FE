5f7c25a030be0185f06b7f42525b6f5d2ad3ba5e
박건규 <geongyu09@gmail.com>
2025-08-06 17:33:59 +0900

    feat: CheckPositionMap에 client only 및 loader 적용
    #80

--- src/app/check-position/page.tsx
+++ src/app/check-position/page.tsx
@@ -1,10 +1,5 @@
 import CheckPositionMap from "@pages/check-position/CheckPositionMap";
-import { Suspense } from "@suspensive/react";
 
 export default function CheckPositionPage() {
-  return (
-    <Suspense>
-      <CheckPositionMap />
-    </Suspense>
-  );
+  return <CheckPositionMap />;
 }
