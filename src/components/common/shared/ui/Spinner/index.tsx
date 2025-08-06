8c14bf2fa87b6d968cf8ad7568e52efccfad4a35
박건규 <geongyu09@gmail.com>
2025-08-06 16:25:50 +0900

    feat: Spinner 컴포넌트의 border width값이 사이즈에 맞도록 수정
    #80

--- src/components/common/shared/ui/Spinner/index.tsx
+++ src/components/common/shared/ui/Spinner/index.tsx
@@ -3,14 +3,14 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/cn";
 
 export const SpinnerVariants = cva(
-  `rounded-full border-4 border-t-transparent animate-spin`,
+  `rounded-full border-t-transparent animate-spin`,
   {
     variants: {
       size: {
-        sm: "w-4 h-4",
-        md: "w-8 h-8",
-        lg: "w-12 h-12",
-        xl: "w-16 h-16",
+        sm: "w-4 h-4 border-2",
+        md: "w-8 h-8 border-3",
+        lg: "w-12 h-12 border-4",
+        xl: "w-16 h-16 border-4",
       },
     },
     defaultVariants: {
