e20816139df4763011fd26864874841574f0d3c1
박건규 <geongyu09@gmail.com>
2025-08-07 16:57:49 +0900

    feat: MapHeaderNavbar 컴포넌트를 suspense로 사용하던 곳에서 감싸진 코드를 컴포넌트 내에서 hoc로 사용하도록 수정
    #83

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,16 +1,13 @@
-import Spacing from "@shared/layout/Spacing";
-import { Suspense } from "react";
-import MapWithHeaderSection from "@/components/features/widgets/map/MapWithHeaderSection";
 import MapHeaderNavbar from "@entities/MapHeaderNavbar";
+import Spacing from "@shared/layout/Spacing";
+import MapWithHeaderSection from "@widgets/map/MapWithHeaderSection";
 
 export default function MapPage() {
   return (
     <div className="relative w-screen h-screen px-6">
       <div className="z-10 fixed">
         <Spacing size={8} />
-        <Suspense>
-          <MapHeaderNavbar />
-        </Suspense>
+        <MapHeaderNavbar />
       </div>
       <MapWithHeaderSection />
     </div>
--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,21 +1,18 @@
-import MapWithHeaderSection from "@/components/features/widgets/map/MapWithHeaderSection";
 import MapHeaderNavbar from "@entities/MapHeaderNavbar";
 import TravelMonitorSection from "@pages/travel/TravelMonitorSection";
 import PositionBottom from "@shared/layout/PositionBottom";
 import Spacing from "@shared/layout/Spacing";
-import { Suspense } from "@suspensive/react";
+import MapWithHeaderSection from "@widgets/map/MapWithHeaderSection";
 
 export default function TravelPage() {
   return (
     <div className="px-6 relative w-screen h-screen">
       <div className="z-10 fixed">
         <Spacing size={8} />
-        <Suspense>
-          <MapHeaderNavbar />
-        </Suspense>
+        <MapHeaderNavbar />
       </div>
       <MapWithHeaderSection />
-      <PositionBottom padding={4}>
+      <PositionBottom padding={4} bottom={0}>
         <TravelMonitorSection />
       </PositionBottom>
     </div>
