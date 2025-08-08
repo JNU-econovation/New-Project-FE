f6a9568d38527067556071683c7cc55d4f3570b6
박건규 <geongyu09@gmail.com>
2025-08-08 17:58:48 +0900

    feat:  브리지를 통해서 사용자의 현재 위경도값을 가져오는 useGetCurrentPosition 훅 구현
    #83

---
+++ src/hooks/common/useGetCurrentPosition/index.ts
@@ -0,0 +1,38 @@
+import useGetCurrentPositionBridge from "@/hooks/feature/bridge/useGetCurrentPositionBridge";
+import type { Position } from "@/hooks/feature/bridge/useGetCurrentPositionBridge";
+import { useEffect, useState } from "react";
+
+const DEFAULT_POSITION = { latitude: 36.122769, longitude: 126.996822 };
+
+interface UseGetCurrentPosition {
+  defaultCurrentPointPosition?: { latitude: number; longitude: number };
+}
+
+const useGetCurrentPosition = ({
+  defaultCurrentPointPosition = DEFAULT_POSITION,
+}: UseGetCurrentPosition) => {
+  const [currentPosition, setCurrentPosition] = useState<{
+    latitude: number;
+    longitude: number;
+  }>(defaultCurrentPointPosition);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getCurrentPosition = useGetCurrentPositionBridge();
+
+  const onResponse = ({ coords }: Position) => {
+    const { latitude, longitude } = coords;
+    setCurrentPosition({ latitude, longitude });
+    setIsLoading(false);
+    return { latitude, longitude };
+  };
+
+  useEffect(() => {
+    getCurrentPosition(onResponse);
+    // 무한 루프 방지를 위해 빈 배열을 의존성으로 설정합니다.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { currentPosition, isLoading };
+};
+
+export default useGetCurrentPosition;
