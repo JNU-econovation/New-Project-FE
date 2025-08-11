fcc40c6361baf29da9c251be557af593d0735d7f
박건규 <geongyu09@gmail.com>
2025-08-11 10:03:30 +0900

    feat: TravelMapSection 구현
    #83

--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,20 +1,9 @@
-import MapHeaderNavbar from "@entities/MapHeaderNavbar";
-import TravelMonitorSection from "@pages/travel/TravelMonitorSection";
-import PositionBottom from "@shared/layout/PositionBottom";
-import Spacing from "@shared/layout/Spacing";
-import MapWithHeaderSection from "@widgets/map/MapWithHeaderSection";
+import TravelMapSection from "@pages/travel/TravelMapSection";
 
 export default function TravelPage() {
   return (
     <div className="px-6 relative w-screen h-screen">
-      <div className="z-10 fixed">
-        <Spacing size={8} />
-        <MapHeaderNavbar />
-      </div>
-      <MapWithHeaderSection />
-      <PositionBottom padding={4} bottom={0}>
-        <TravelMonitorSection />
-      </PositionBottom>
+      <TravelMapSection />
     </div>
   );
 }
--- src/components/features/pages/social-login-loading/TokenProcessingSection/index.tsx
+++ src/components/features/pages/social-login-loading/TokenProcessingSection/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useSendToken from "@hooks/common/useSendToken";
-import useSaveAuthToken from "@hooks/feature/useSaveAuthToken";
+import useSaveAuthToken from "@hooks/feature/authenticate/useSaveAuthToken";
 import Flex from "@shared/layout/Flex";
 import Spinner from "@shared/ui/Spinner";
 
