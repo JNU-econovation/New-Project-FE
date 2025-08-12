753b6fd8738094b5c066e54c595535697d581424
박건규 <geongyu09@gmail.com>
2025-08-12 15:22:03 +0900

    fix:: 더이상 사용하지 않는 import 문 주석처리
    #83

--- src/components/common/entities/TanstackQueryProvider/index.tsx
+++ src/components/common/entities/TanstackQueryProvider/index.tsx
@@ -5,7 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function makeQueryClient() {
   return new QueryClient({
